7df4a4113a03845c995197aeebfef94c23ff8424
arotchev <arotchev@yandex-team.ru>
2020-08-07 17:30:12 +0300

    SLYDB-104 ydb go sdk: support Experimental Service
    ref:7186307
    sync: https://proxy.sandbox.yandex-team.ru/1651837725

---
+++ experimental/options.go
@@ -0,0 +1,62 @@
+package experimental
+
+import "github.com/yandex-cloud/ydb-go-sdk/api/protos/Ydb_Experimental"
+
+type StreamQueryProfile uint
+
+const (
+	StreamQueryProfileUnknown StreamQueryProfile = iota
+	StreamQueryProfileNone
+	StreamQueryProfileBasic
+	StreamQueryProfileFull
+)
+
+func (s StreamQueryProfile) String() string {
+	switch s {
+	case StreamQueryProfileNone:
+		return "none"
+	case StreamQueryProfileBasic:
+		return "basic"
+	case StreamQueryProfileFull:
+		return "full"
+	default:
+		return "unknown"
+	}
+}
+
+func (s StreamQueryProfile) toYDB() Ydb_Experimental.ExecuteStreamQueryRequest_ProfileMode {
+	switch s {
+	case StreamQueryProfileNone:
+		return Ydb_Experimental.ExecuteStreamQueryRequest_NONE
+	case StreamQueryProfileBasic:
+		return Ydb_Experimental.ExecuteStreamQueryRequest_BASIC
+	case StreamQueryProfileFull:
+		return Ydb_Experimental.ExecuteStreamQueryRequest_FULL
+	default:
+		return Ydb_Experimental.ExecuteStreamQueryRequest_PROFILE_MODE_UNSPECIFIED
+	}
+}
+
+func streamQueryProfile(s Ydb_Experimental.ExecuteStreamQueryRequest_ProfileMode) StreamQueryProfile {
+	switch s {
+	case Ydb_Experimental.ExecuteStreamQueryRequest_NONE:
+		return StreamQueryProfileNone
+	case Ydb_Experimental.ExecuteStreamQueryRequest_BASIC:
+		return StreamQueryProfileBasic
+	case Ydb_Experimental.ExecuteStreamQueryRequest_FULL:
+		return StreamQueryProfileFull
+	default:
+		return StreamQueryProfileUnknown
+	}
+}
+
+type (
+	streamQueryDesc   Ydb_Experimental.ExecuteStreamQueryRequest
+	StreamQueryOption func(s *streamQueryDesc)
+)
+
+func WithStreamQueryProfile(profile StreamQueryProfile) StreamQueryOption {
+	return func(s *streamQueryDesc) {
+		s.ProfileMode = profile.toYDB()
+	}
+}
---
+++ experimental/result_test.go
@@ -0,0 +1,42 @@
+package experimental
+
+import (
+	"testing"
+
+	"github.com/yandex-cloud/ydb-go-sdk/api/protos/Ydb"
+	"github.com/yandex-cloud/ydb-go-sdk/api/protos/Ydb_Experimental"
+)
+
+func TestResultAny(t *testing.T) {
+	var ydbResult Ydb_Experimental.ExecuteStreamQueryResult
+
+	ydbResult = Ydb_Experimental.ExecuteStreamQueryResult{
+		Result: &Ydb_Experimental.ExecuteStreamQueryResult_ResultSet{
+			ResultSet: &Ydb.ResultSet{},
+		},
+	}
+	r := streamQueryResult(&ydbResult)
+	if r.ResultType != StreamQueryResultTypeResultSet && r.ResultSet() == nil {
+		t.Error("Unexpected execute query result")
+	}
+
+	ydbResult = Ydb_Experimental.ExecuteStreamQueryResult{
+		Result: &Ydb_Experimental.ExecuteStreamQueryResult_Profile{
+			Profile: "myProfile",
+		},
+	}
+	r = streamQueryResult(&ydbResult)
+	if r.ResultType != StreamQueryResultTypeProfile && r.Profile() == nil && *r.Profile() != "myProfile" {
+		t.Error("Unexpected execute query result")
+	}
+
+	ydbResult = Ydb_Experimental.ExecuteStreamQueryResult{
+		Result: &Ydb_Experimental.ExecuteStreamQueryResult_Progress{
+			Progress: &Ydb_Experimental.StreamQueryProgress{},
+		},
+	}
+	r = streamQueryResult(&ydbResult)
+	if r.ResultType != StreamQueryResultTypeProgress && r.Progress() == nil {
+		t.Error("Unexpected execute query result")
+	}
+}
