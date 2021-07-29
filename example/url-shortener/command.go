a7c878f9cb10d96175a740f2ab131baaefebcb2a
asmyasnikov <asmyasnikov@yandex-team.ru>
2021-07-29 19:28:59 +0300

    KIKIMR-12567 url-shortener
    KIKIMR-12567 url-shortener server
    
    ref:8469732
    sync: https://proxy.sandbox.yandex-team.ru/2317217958

---
+++ example/url-shortener/command.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/yandex-cloud/ydb-go-sdk/example/internal/cli"
+	"context"
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type Command struct {
+	port int
+}
+
+func (cmd *Command) Run(ctx context.Context, parameters cli.Parameters) error {
+	h, err := NewHandler(ctx, parameters.Endpoint, parameters.Database, parameters.TLS)
+	if err != nil {
+		return fmt.Errorf("error on create handler: %w", err)
+	}
+	fmt.Printf("Running server on port %d...\n", cmd.port)
+	return http.ListenAndServe(":"+strconv.Itoa(cmd.port), http.HandlerFunc(h.Handle))
+}
+
+func (cmd *Command) ExportFlags(ctx context.Context, flagSet *flag.FlagSet) {
+	flagSet.IntVar(&cmd.port, "port", 80, "http port for web-server")
+}
---
+++ example/url-shortener/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/yandex-cloud/ydb-go-sdk/example/internal/cli"
+)
+
+
+func main() {
+	cli.Run(new(Command))
+}
