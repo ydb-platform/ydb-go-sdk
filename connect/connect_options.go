21c16693c4c86b5d853c66206180192be3cb093b
asmyasnikov <79263256394@ya.ru>
2021-09-06 12:49:09 +0300

    changes of connect options

--- connect/connect_options.go
+++ connect/connect_options.go
@@ -1,12 +1,13 @@
 package connect
 
 import (
+	"context"
 	"time"
 
 	"github.com/YandexDatabase/ydb-go-sdk/v3"
 )
 
-type Option func(client *Connection) error
+type Option func(ctx context.Context, client *Connection) error
 
 func WithAccessTokenCredentials(accessToken string) Option {
 	return WithCredentials(
@@ -20,12 +21,12 @@ func WithAnonymousCredentials() Option {
 	)
 }
 
-func WithCreateCredentialsFunc(createCredentials func() (ydb.Credentials, error)) Option {
-	return func(c *Connection) error {
+func WithCreateCredentialsFunc(createCredentials func(ctx context.Context) (ydb.Credentials, error)) Option {
+	return func(ctx context.Context, c *Connection) error {
 		if c.driverConfig == nil {
 			c.driverConfig = &ydb.DriverConfig{}
 		}
-		credentials, err := createCredentials()
+		credentials, err := createCredentials(ctx)
 		if err != nil {
 			return err
 		}
@@ -35,20 +36,20 @@ func WithCreateCredentialsFunc(createCredentials func() (ydb.Credentials, error)
 }
 
 func WithCredentials(credentials ydb.Credentials) Option {
-	return WithCreateCredentialsFunc(func() (ydb.Credentials, error) {
+	return WithCreateCredentialsFunc(func(context.Context) (ydb.Credentials, error) {
 		return credentials, nil
 	})
 }
 
 func WithDriverConfig(config *ydb.DriverConfig) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.driverConfig = config
 		return nil
 	}
 }
 
 func WithDiscoveryInterval(discoveryInterval time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		if c.driverConfig == nil {
 			c.driverConfig = &ydb.DriverConfig{}
 		}
@@ -58,49 +59,49 @@ func WithDiscoveryInterval(discoveryInterval time.Duration) Option {
 }
 
 func WithSessionPoolSizeLimit(sizeLimit int) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.SizeLimit = sizeLimit
 		return nil
 	}
 }
 
 func WithSessionPoolKeepAliveMinSize(keepAliveMinSize int) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.KeepAliveMinSize = keepAliveMinSize
 		return nil
 	}
 }
 
 func WithSessionPoolIdleThreshold(idleThreshold time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.IdleThreshold = idleThreshold
 		return nil
 	}
 }
 
 func WithSessionPoolBusyCheckInterval(busyCheckInterval time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.BusyCheckInterval = busyCheckInterval
 		return nil
 	}
 }
 
 func WithSessionPoolKeepAliveTimeout(keepAliveTimeout time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.KeepAliveTimeout = keepAliveTimeout
 		return nil
 	}
 }
 
 func WithSessionPoolCreateSessionTimeout(createSessionTimeout time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.CreateSessionTimeout = createSessionTimeout
 		return nil
 	}
 }
 
 func WithSessionPoolDeleteTimeout(deleteTimeout time.Duration) Option {
-	return func(c *Connection) error {
+	return func(ctx context.Context, c *Connection) error {
 		c.table.sessionPool.DeleteTimeout = deleteTimeout
 		return nil
 	}
