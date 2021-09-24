a67005b20f347aa65779fcd517cdc9be5116f975
asmyasnikov <79263256394@ya.ru>
2021-09-25 00:12:00 +0300

    fix test

--- connect_params_test.go
+++ connect_params_test.go
@@ -16,28 +16,28 @@ func TestParseConnectionString(t *testing.T) {
 		error            error
 	}{
 		{
-			"grpc://ydb-ru.yandex.net:2135/?name=/ru/home/gvit/mydb",
+			"grpc://ydb-ru.yandex.net:2135/?database=/ru/home/gvit/mydb",
 			"grpc",
 			"ydb-ru.yandex.net:2135",
 			"/ru/home/gvit/mydb",
 			nil,
 		},
 		{
-			"grpcs://ydb.serverless.yandexcloud.net:2135/?name=/ru-central1/b1g8skpblkos03malf3s/etn02qso4v3isjb00te1",
+			"grpcs://ydb.serverless.yandexcloud.net:2135/?database=/ru-central1/b1g8skpblkos03malf3s/etn02qso4v3isjb00te1",
 			"grpcs",
 			"ydb.serverless.yandexcloud.net:2135",
 			"/ru-central1/b1g8skpblkos03malf3s/etn02qso4v3isjb00te1",
 			nil,
 		},
 		{
-			"grpcs://lb.etn03r9df42nb631unbv.ydb.mdb.yandexcloud.net:2135/?name=/ru-central1/b1g8skpblkos03malf3s/etn03r9df42nb631unbv",
+			"grpcs://lb.etn03r9df42nb631unbv.ydb.mdb.yandexcloud.net:2135/?database=/ru-central1/b1g8skpblkos03malf3s/etn03r9df42nb631unbv",
 			"grpcs",
 			"lb.etn03r9df42nb631unbv.ydb.mdb.yandexcloud.net:2135",
 			"/ru-central1/b1g8skpblkos03malf3s/etn03r9df42nb631unbv",
 			nil,
 		},
 		{
-			"abcd://ydb-ru.yandex.net:2135/?name=/ru/home/gvit/mydb",
+			"abcd://ydb-ru.yandex.net:2135/?database=/ru/home/gvit/mydb",
 			"",
 			"",
 			"",
@@ -76,6 +76,6 @@ func TestMustConnectionString(t *testing.T) {
 			t.Fatal("panic on MustConnectionString", e)
 		}
 	}()
-	params := MustConnectionString("grpcs://endpoint/?name=name")
+	params := MustConnectionString("grpcs://endpoint/?database=name")
 	assertConnectParams(t, params)
 }
