f10819ca67754d33b74a0f061385f0990c1ea3a8
asmyasnikov <asmyasnikov@yandex-team.ru>
2021-09-27 19:08:12 +0000

    hotfix: context idempotent operation
    context idempotent operation
    
    ref:8675785
    sync: https://proxy.sandbox.yandex-team.ru/2457788194

--- table/retry.go
+++ table/retry.go
@@ -156,12 +156,12 @@ func (r Retryer) backoff(ctx context.Context, m ydb.RetryMode, i int) error {
 // Do calls op.Do until it return nil or not retriable error.
 func (r Retryer) Do(ctx context.Context, op Operation) (err error) {
 	var (
-		s                  *Session
-		m                  ydb.RetryMode
-		i                  int
-		start              = time.Now()
-		retryNoIdempotent  = ydb.ContextRetryNoIdempotent(ctx)
-		retryTraceLoopDone = retryTraceOnLoop(ctx, r.Trace, ctx)
+		s                     *Session
+		m                     ydb.RetryMode
+		i                     int
+		start                 = time.Now()
+		isOperationIdempotent = ydb.IsOperationIdempotent(ctx)
+		retryTraceLoopDone    = retryTraceOnLoop(ctx, r.Trace, ctx)
 	)
 	defer func() {
 		retryTraceLoopDone(ctx, time.Since(start), i)
@@ -202,7 +202,7 @@ func (r Retryer) Do(ctx context.Context, op Operation) (err error) {
 				_ = r.SessionProvider.CloseSession(ctx, s)
 				s = nil
 			}
-			if !m.MustRetry(retryNoIdempotent) {
+			if !m.MustRetry(isOperationIdempotent) {
 				return err
 			}
 			if e := r.backoff(ctx, m, i); e != nil {
--- version.go
+++ version.go
@@ -1,5 +1,5 @@
 package ydb
 
 const (
-	Version = "ydb-go-sdk/2.9.4"
+	Version = "ydb-go-sdk/2.9.5"
 )
