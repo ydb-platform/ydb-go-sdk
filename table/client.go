e818c4aaeb6b924a660ce1902943e320b753c80a
asmyasnikov <79263256394@ya.ru>
2021-10-22 14:49:35 +0300

    Add comments to table client API interface

--- table/client.go
+++ table/client.go
@@ -8,7 +8,21 @@ import (
 type RetryOperation func(context.Context, Session) (err error)
 
 type Client interface {
-	RetryIdempotent(ctx context.Context, op RetryOperation) (err error)
-	RetryNonIdempotent(ctx context.Context, op RetryOperation) (err error)
 	Close(ctx context.Context) error
+
+	// RetryIdempotent retries the retry operation as a idempotent
+	// operation (i.e. the operation can't change the state of the database)
+	// while the operation returns an error
+	//
+	// This is an experimental API that can be changed without changing
+	// the major version.
+	RetryIdempotent(ctx context.Context, op RetryOperation) (err error)
+
+	// RetryNonIdempotent retries the retry operation as a non-idempotent
+	// operation (i.e. the operation can change the state of the database)
+	// while the operation returns an error
+	//
+	// This is an experimental API that can be changed without changing
+	// the major version.
+	RetryNonIdempotent(ctx context.Context, op RetryOperation) (err error)
 }
