edd04c853b6e534dcd3f07545cd8c1024bd7a107
asmyasnikov <79263256394@ya.ru>
2021-12-18 23:18:03 +0300

    nop Rollback for committed transactions

--- internal/table/session_test.go
+++ internal/table/session_test.go
@@ -8,9 +8,11 @@ import (
 	"time"
 
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/ydb-platform/ydb-go-genproto/Ydb_Table_V1"
 	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb"
+	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb_Operations"
 	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb_Scheme"
 	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb_Table"
 
@@ -469,5 +471,142 @@ func TestQueryCachePolicyKeepInCache(t *testing.T) {
 				t.Fatal(err)
 			}
 		})
+	}
+}
+
+func TestTxSkipRollbackForCommitted(t *testing.T) {
+	var (
+		begin    = 0
+		commit   = 0
+		rollback = 0
+	)
+	b := StubBuilder{
+		T: t,
+		Cluster: testutil.NewCluster(
+			testutil.WithInvokeHandlers(
+				testutil.InvokeHandlers{
+					// nolint:unparam
+					testutil.TableBeginTransaction: func(request interface{}) (proto.Message, error) {
+						_, ok := request.(*Ydb_Table.BeginTransactionRequest)
+						if !ok {
+							t.Fatalf("cannot cast request '%T' to *Ydb_Table.BeginTransactionRequest", request)
+						}
+						result, err := anypb.New(
+							&Ydb_Table.BeginTransactionResult{
+								TxMeta: &Ydb_Table.TransactionMeta{
+									Id: "",
+								},
+							},
+						)
+						if err != nil {
+							return nil, err
+						}
+						begin++
+						return &Ydb_Table.BeginTransactionResponse{
+							Operation: &Ydb_Operations.Operation{
+								Ready:  true,
+								Status: Ydb.StatusIds_SUCCESS,
+								Result: result,
+							},
+						}, nil
+					},
+					// nolint:unparam
+					testutil.TableCommitTransaction: func(request interface{}) (proto.Message, error) {
+						_, ok := request.(*Ydb_Table.CommitTransactionRequest)
+						if !ok {
+							t.Fatalf("cannot cast request '%T' to *Ydb_Table.CommitTransactionRequest", request)
+						}
+						result, err := anypb.New(
+							&Ydb_Table.CommitTransactionResult{},
+						)
+						if err != nil {
+							return nil, err
+						}
+						commit++
+						return &Ydb_Table.CommitTransactionResponse{
+							Operation: &Ydb_Operations.Operation{
+								Ready:  true,
+								Status: Ydb.StatusIds_SUCCESS,
+								Result: result,
+							},
+						}, nil
+					},
+					// nolint:unparam
+					testutil.TableRollbackTransaction: func(request interface{}) (proto.Message, error) {
+						_, ok := request.(*Ydb_Table.RollbackTransactionRequest)
+						if !ok {
+							t.Fatalf("cannot cast request '%T' to *Ydb_Table.RollbackTransactionRequest", request)
+						}
+						rollback++
+						return &Ydb_Table.RollbackTransactionResponse{
+							Operation: &Ydb_Operations.Operation{
+								Ready:  true,
+								Status: Ydb.StatusIds_SUCCESS,
+							},
+						}, nil
+					},
+					// nolint:unparam
+					testutil.TableCreateSession: func(request interface{}) (result proto.Message, err error) {
+						return &Ydb_Table.CreateSessionResult{
+							SessionId: testutil.SessionID(),
+						}, nil
+					},
+				},
+			),
+		),
+	}
+	s, err := b.createSession(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	{
+		x, err := s.BeginTransaction(context.Background(), table.TxSettings())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if begin != 1 {
+			t.Fatalf("unexpected begin: %d", begin)
+		}
+		_, err = x.CommitTx(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if commit != 1 {
+			t.Fatalf("unexpected commit: %d", begin)
+		}
+		_, _ = x.CommitTx(context.Background())
+		if commit != 1 {
+			t.Fatalf("unexpected commit: %d", begin)
+		}
+		err = x.Rollback(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if rollback != 0 {
+			t.Fatalf("unexpected rollback: %d", begin)
+		}
+	}
+	{
+		x, err := s.BeginTransaction(context.Background(), table.TxSettings())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if begin != 2 {
+			t.Fatalf("unexpected begin: %d", begin)
+		}
+		err = x.Rollback(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if rollback != 1 {
+			t.Fatalf("unexpected rollback: %d", begin)
+		}
+		_, err = x.CommitTx(context.Background())
+		if err != nil {
+			t.Fatal(err)
+		}
+		if commit != 2 {
+			t.Fatalf("unexpected commit: %d", begin)
+		}
 	}
 }
