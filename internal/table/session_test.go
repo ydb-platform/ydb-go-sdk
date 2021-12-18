cfdaaddeed1b5af6f589f713a0134e1043d5cff9
asmyasnikov <79263256394@ya.ru>
2021-12-18 19:04:48 +0300

    Add TestQueryCachePolicyKeepInCache

--- internal/table/session_test.go
+++ internal/table/session_test.go
@@ -398,3 +398,76 @@ func TestSessionOperationModeOnExecuteDataQuery(t *testing.T) {
 		)
 	}
 }
+
+func TestQueryCachePolicyKeepInCache(t *testing.T) {
+	for _, test := range [...]struct {
+		name                   string
+		queryCachePolicyOption []options.QueryCachePolicyOption
+	}{
+		{
+			name:                   "with server cache",
+			queryCachePolicyOption: []options.QueryCachePolicyOption{options.WithQueryCachePolicyKeepInCache()},
+		},
+		{
+			name:                   "no server cache",
+			queryCachePolicyOption: []options.QueryCachePolicyOption{},
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			b := StubBuilder{
+				T: t,
+				Cluster: testutil.NewCluster(
+					testutil.WithInvokeHandlers(
+						testutil.InvokeHandlers{
+							// nolint:unparam
+							testutil.TableExecuteDataQuery: func(request interface{}) (proto.Message, error) {
+								r, ok := request.(*Ydb_Table.ExecuteDataQueryRequest)
+								if !ok {
+									t.Fatalf("cannot cast request '%T' to *Ydb_Table.ExecuteDataQueryRequest", request)
+								}
+								if len(test.queryCachePolicyOption) > 0 {
+									if !r.QueryCachePolicy.GetKeepInCache() {
+										t.Fatalf("keep-in-cache policy must be true, got: %v", r.QueryCachePolicy.GetKeepInCache())
+									}
+								} else {
+									if r.QueryCachePolicy.GetKeepInCache() {
+										t.Fatalf("keep-in-cache policy must be false, got: %v", r.QueryCachePolicy.GetKeepInCache())
+									}
+								}
+								return &Ydb_Table.ExecuteQueryResult{
+									TxMeta: &Ydb_Table.TransactionMeta{
+										Id: "",
+									},
+								}, nil
+							},
+							// nolint:unparam
+							testutil.TableCreateSession: func(request interface{}) (result proto.Message, err error) {
+								return &Ydb_Table.CreateSessionResult{
+									SessionId: testutil.SessionID(),
+								}, nil
+							},
+						},
+					),
+				),
+			}
+			s, err := b.createSession(context.Background())
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, _, err = s.Execute(
+				context.Background(), table.TxControl(
+					table.BeginTx(
+						table.WithOnlineReadOnly(),
+					),
+					table.CommitTx(),
+				),
+				"SELECT 1",
+				table.NewQueryParameters(),
+				options.WithQueryCachePolicy(test.queryCachePolicyOption...),
+			)
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
