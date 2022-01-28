c97fa8b6759a6f42b2c3c09586df8e3489bcc81a
asmyasnikov <79263256394@ya.ru>
2022-01-28 13:41:20 +0300

    use io/ioutil for go less then 1.16

--- internal/file/file.go
+++ internal/file/file.go
@@ -4,53 +4,9 @@
 package file
 
 import (
-	"io"
-	"os"
+	"io/ioutil"
 )
 
-// Read reads the named file and returns the contents.
-// A successful call returns err == nil, not err == EOF.
-// Because Read reads the whole file, it does not treat an EOF from Read
-// as an error to be reported.
-// Read is a copy-paste of os.ReadFile for support go under 1.15
 func Read(name string) ([]byte, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var size int
-	if info, err := f.Stat(); err == nil {
-		size64 := info.Size()
-		if int64(int(size64)) == size64 {
-			size = int(size64)
-		}
-	}
-	size++ // one byte for final read at EOF
-
-	// If a file claims a small size, read at least 512 bytes.
-	// In particular, files in Linux's /proc claim size 0 but
-	// then do not work right if read in small pieces,
-	// so an initial read of 1 byte would not work correctly.
-	if size < 512 {
-		size = 512
-	}
-
-	data := make([]byte, 0, size)
-	for {
-		if len(data) >= cap(data) {
-			// nolint:gocritic
-			d := append(data[:cap(data)], 0)
-			data = d[:len(data)]
-		}
-		n, err := f.Read(data[len(data):cap(data)])
-		data = data[:len(data)+n]
-		if err != nil {
-			if err == io.EOF {
-				err = nil
-			}
-			return data, err
-		}
-	}
+	return ioutil.ReadFile(name)
 }
