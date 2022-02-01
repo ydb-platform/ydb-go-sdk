fe60adbf47d8fd0f723ac10b26ae2d6f56e084af
asmyasnikov <79263256394@ya.ru>
2022-02-01 09:34:00 +0300

    about go1.18

--- table/result/indexed/indexed.go
+++ table/result/indexed/indexed.go
@@ -1,5 +1,8 @@
 package indexed
 
 // Value is a type scan destination of ydb values
-// This is a proxy type for preparing go1.18 embedded types
+// This is a proxy type for preparing go1.18 type set constrains such as
+// type Value interface {
+//   ~int8 | ~int64 | ~string
+// }
 type Value interface{}
