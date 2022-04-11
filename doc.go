20db23ec092e98605437707bb8234b6a392180cb
asmyasnikov <79263256394@ya.ru>
2022-04-11 18:39:29 +0300

    godoc

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package ydb-go-sdk - native Go's driver for YDB.
+/*
+Supports table, discovery, coordination, ratelimiter, scheme and scripting clients for YDB.
+YDB is an open-source Distributed SQL Database that combines high availability and scalability with strict consistency and ACID transactions.
+*/
+package ydb
