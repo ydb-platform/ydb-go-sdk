1b48c80bb67e9c173d946277a7d2f0e58ff0239f
asmyasnikov <79263256394@ya.ru>
2022-06-17 20:10:10 +0300

    optimize allocations

--- internal/value/exp/allocator/allocator.go
+++ internal/value/exp/allocator/allocator.go
@@ -8,239 +8,301 @@ import (
 )
 
 type (
-	putter interface {
-		Put()
-	}
-	free struct {
-		v any
-		p *sync.Pool
-	}
 	Allocator struct {
-		allocations []free
+		valueAllocations         []*Ydb.Value
+		typeAllocations          []*Ydb.Type
+		typePrimitiveAllocations []*Ydb.Type_TypeId
+		typeDecimalAllocations   []*Ydb.Type_DecimalType
+		typeListAllocations      []*Ydb.Type_ListType
+		typeEmptyListAllocations []*Ydb.Type_EmptyListType
+		typeTupleAllocations     []*Ydb.Type_TupleType
+		decimalAllocations       []*Ydb.DecimalType
+		listAllocations          []*Ydb.ListType
+		tupleAllocations         []*Ydb.TupleType
+		typeOptionalAllocations  []*Ydb.Type_OptionalType
+		typedValueAllocations    []*Ydb.TypedValue
+		boolAllocations          []*Ydb.Value_BoolValue
+		bytesAllocations         []*Ydb.Value_BytesValue
+		textAllocations          []*Ydb.Value_TextValue
+		int32Allocations         []*Ydb.Value_Int32Value
+		uint32Allocations        []*Ydb.Value_Uint32Value
+		low128Allocations        []*Ydb.Value_Low_128
+		int64Allocations         []*Ydb.Value_Int64Value
+		uint64Allocations        []*Ydb.Value_Uint64Value
+		floatAllocations         []*Ydb.Value_FloatValue
+		doubleAllocations        []*Ydb.Value_DoubleValue
 	}
-	pool[T any] struct {
-		sync.Pool
-	}
+	pool[T any] sync.Pool
 )
 
-func (p *pool[T]) Get() (v *T) {
-	defer func() {
-		if vv, ok := (any)(v).(interface {
-			Reset()
-		}); ok {
-			vv.Reset()
-		}
-	}()
-	return p.Pool.Get().(*T)
+func (p *pool[T]) Get() *T {
+	v := (*sync.Pool)(p).Get()
+	if v == nil {
+		var zero T
+		v = &zero
+	}
+	if vv, ok := (v).(interface {
+		Reset()
+	}); ok {
+		vv.Reset()
+	}
+	return v.(*T)
 }
 
 func (p *pool[T]) Put(t *T) {
-	p.Pool.Put(t)
-}
-
-func makePool[T any]() *pool[T] {
-	return &pool[T]{
-		sync.Pool{
-			New: func() any {
-				return new(T)
-			},
-		},
-	}
+	(*sync.Pool)(p).Put(t)
 }
 
 var (
-	allocatorsPool    = makePool[Allocator]()
-	valuePool         = makePool[Ydb.Value]()
-	typePool          = makePool[Ydb.Type]()
-	typePrimitivePool = makePool[Ydb.Type_TypeId]()
-	typeDecimalPool   = makePool[Ydb.Type_DecimalType]()
-	typeListPool      = makePool[Ydb.Type_ListType]()
-	typeEmptyListPool = makePool[Ydb.Type_EmptyListType]()
-	typeTuplePool     = makePool[Ydb.Type_TupleType]()
-	decimalPool       = makePool[Ydb.DecimalType]()
-	listPool          = makePool[Ydb.ListType]()
-	tuplePool         = makePool[Ydb.TupleType]()
-	typeOptionalPool  = makePool[Ydb.Type_OptionalType]()
-	typedValuePool    = makePool[Ydb.TypedValue]()
-	boolPool          = makePool[Ydb.Value_BoolValue]()
-	bytesPool         = makePool[Ydb.Value_BytesValue]()
-	textPool          = makePool[Ydb.Value_TextValue]()
-	int32Pool         = makePool[Ydb.Value_Int32Value]()
-	uint32Pool        = makePool[Ydb.Value_Uint32Value]()
-	low128Pool        = makePool[Ydb.Value_Low_128]()
-	int64Pool         = makePool[Ydb.Value_Int64Value]()
-	uint64Pool        = makePool[Ydb.Value_Uint64Value]()
-	floatPool         = makePool[Ydb.Value_FloatValue]()
-	doublePool        = makePool[Ydb.Value_DoubleValue]()
+	allocatorsPool    pool[Allocator]
+	valuePool         pool[Ydb.Value]
+	typePool          pool[Ydb.Type]
+	typePrimitivePool pool[Ydb.Type_TypeId]
+	typeDecimalPool   pool[Ydb.Type_DecimalType]
+	typeListPool      pool[Ydb.Type_ListType]
+	typeEmptyListPool pool[Ydb.Type_EmptyListType]
+	typeTuplePool     pool[Ydb.Type_TupleType]
+	decimalPool       pool[Ydb.DecimalType]
+	listPool          pool[Ydb.ListType]
+	tuplePool         pool[Ydb.TupleType]
+	typeOptionalPool  pool[Ydb.Type_OptionalType]
+	typedValuePool    pool[Ydb.TypedValue]
+	boolPool          pool[Ydb.Value_BoolValue]
+	bytesPool         pool[Ydb.Value_BytesValue]
+	textPool          pool[Ydb.Value_TextValue]
+	int32Pool         pool[Ydb.Value_Int32Value]
+	uint32Pool        pool[Ydb.Value_Uint32Value]
+	low128Pool        pool[Ydb.Value_Low_128]
+	int64Pool         pool[Ydb.Value_Int64Value]
+	uint64Pool        pool[Ydb.Value_Uint64Value]
+	floatPool         pool[Ydb.Value_FloatValue]
+	doublePool        pool[Ydb.Value_DoubleValue]
 )
 
-func New() *Allocator {
+func New() (v *Allocator) {
 	return allocatorsPool.Get()
 }
 
-func (a *Allocator) Close() {
-	var l int
-	for {
-		l = len(a.allocations)
-		if l == 0 {
-			break
-		}
-		a.allocations[l-1].p.Put(a.allocations[l-1].v)
-		a.allocations = a.allocations[:l-1]
+func (a *Allocator) Free() {
+	for _, v := range a.valueAllocations {
+		valuePool.Put(v)
 	}
+	a.valueAllocations = a.valueAllocations[:0]
+	for _, v := range a.typeAllocations {
+		typePool.Put(v)
+	}
+	a.typeAllocations = a.typeAllocations[:0]
+	for _, v := range a.typePrimitiveAllocations {
+		typePrimitivePool.Put(v)
+	}
+	a.typePrimitiveAllocations = a.typePrimitiveAllocations[:0]
+	for _, v := range a.typeDecimalAllocations {
+		typeDecimalPool.Put(v)
+	}
+	a.typeDecimalAllocations = a.typeDecimalAllocations[:0]
+	for _, v := range a.typeListAllocations {
+		typeListPool.Put(v)
+	}
+	a.typeListAllocations = a.typeListAllocations[:0]
+	for _, v := range a.typeEmptyListAllocations {
+		typeEmptyListPool.Put(v)
+	}
+	a.typeEmptyListAllocations = a.typeEmptyListAllocations[:0]
+	for _, v := range a.typeTupleAllocations {
+		typeTuplePool.Put(v)
+	}
+	a.typeTupleAllocations = a.typeTupleAllocations[:0]
+	for _, v := range a.decimalAllocations {
+		decimalPool.Put(v)
+	}
+	a.decimalAllocations = a.decimalAllocations[:0]
+	for _, v := range a.listAllocations {
+		listPool.Put(v)
+	}
+	a.listAllocations = a.listAllocations[:0]
+	for _, v := range a.tupleAllocations {
+		tuplePool.Put(v)
+	}
+	a.tupleAllocations = a.tupleAllocations[:0]
+	for _, v := range a.typeOptionalAllocations {
+		typeOptionalPool.Put(v)
+	}
+	a.typeOptionalAllocations = a.typeOptionalAllocations[:0]
+	for _, v := range a.typedValueAllocations {
+		typedValuePool.Put(v)
+	}
+	a.typedValueAllocations = a.typedValueAllocations[:0]
+	for _, v := range a.boolAllocations {
+		boolPool.Put(v)
+	}
+	a.boolAllocations = a.boolAllocations[:0]
+	for _, v := range a.bytesAllocations {
+		bytesPool.Put(v)
+	}
+	a.bytesAllocations = a.bytesAllocations[:0]
+	for _, v := range a.textAllocations {
+		textPool.Put(v)
+	}
+	a.textAllocations = a.textAllocations[:0]
+	for _, v := range a.int32Allocations {
+		int32Pool.Put(v)
+	}
+	a.int32Allocations = a.int32Allocations[:0]
+	for _, v := range a.uint32Allocations {
+		uint32Pool.Put(v)
+	}
+	a.uint32Allocations = a.uint32Allocations[:0]
+	for _, v := range a.low128Allocations {
+		low128Pool.Put(v)
+	}
+	a.low128Allocations = a.low128Allocations[:0]
+	for _, v := range a.int64Allocations {
+		int64Pool.Put(v)
+	}
+	a.int64Allocations = a.int64Allocations[:0]
+	for _, v := range a.uint64Allocations {
+		uint64Pool.Put(v)
+	}
+	a.uint64Allocations = a.uint64Allocations[:0]
+	for _, v := range a.floatAllocations {
+		floatPool.Put(v)
+	}
+	a.floatAllocations = a.floatAllocations[:0]
+	for _, v := range a.doubleAllocations {
+		doublePool.Put(v)
+	}
+	a.doubleAllocations = a.doubleAllocations[:0]
 	allocatorsPool.Put(a)
 }
 
 func (a *Allocator) Value() (v *Ydb.Value) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &valuePool.Pool})
-	}()
-	return valuePool.Get()
+	v = valuePool.Get()
+	a.valueAllocations = append(a.valueAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypedValue() (v *Ydb.TypedValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typedValuePool.Pool})
-	}()
-	return typedValuePool.Get()
+	v = typedValuePool.Get()
+	a.typedValueAllocations = append(a.typedValueAllocations, v)
+	return v
 }
 
 func (a *Allocator) Type() (v *Ydb.Type) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typePool.Pool})
-	}()
-	return typePool.Get()
+	v = typePool.Get()
+	a.typeAllocations = append(a.typeAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypePrimitive() (v *Ydb.Type_TypeId) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typePrimitivePool.Pool})
-	}()
-	return typePrimitivePool.Get()
+	v = typePrimitivePool.Get()
+	a.typePrimitiveAllocations = append(a.typePrimitiveAllocations, v)
+	return v
 }
 
 func (a *Allocator) Decimal() (v *Ydb.DecimalType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &decimalPool.Pool})
-	}()
-	return decimalPool.Get()
+	v = decimalPool.Get()
+	a.decimalAllocations = append(a.decimalAllocations, v)
+	return v
 }
 
 func (a *Allocator) List() (v *Ydb.ListType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &listPool.Pool})
-	}()
-	return listPool.Get()
+	v = listPool.Get()
+	a.listAllocations = append(a.listAllocations, v)
+	return v
 }
 
 func (a *Allocator) Tuple() (v *Ydb.TupleType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &tuplePool.Pool})
-	}()
-	return tuplePool.Get()
+	v = tuplePool.Get()
+	a.tupleAllocations = append(a.tupleAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypeDecimal() (v *Ydb.Type_DecimalType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typeDecimalPool.Pool})
-	}()
-	return typeDecimalPool.Get()
+	v = typeDecimalPool.Get()
+	a.typeDecimalAllocations = append(a.typeDecimalAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypeList() (v *Ydb.Type_ListType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typeListPool.Pool})
-	}()
-	return typeListPool.Get()
+	v = typeListPool.Get()
+	a.typeListAllocations = append(a.typeListAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypeTuple() (v *Ydb.Type_TupleType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typeTuplePool.Pool})
-	}()
-	return typeTuplePool.Get()
+	v = typeTuplePool.Get()
+	a.typeTupleAllocations = append(a.typeTupleAllocations, v)
+	return v
 }
 
 func (a *Allocator) EmptyTypeList() (v *Ydb.Type_EmptyListType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typeEmptyListPool.Pool})
-	}()
-	return typeEmptyListPool.Get()
+	v = typeEmptyListPool.Get()
+	a.typeEmptyListAllocations = append(a.typeEmptyListAllocations, v)
+	return v
 }
 
 func (a *Allocator) TypeOptional() (v *Ydb.Type_OptionalType) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &typeOptionalPool.Pool})
-	}()
-	return typeOptionalPool.Get()
+	v = typeOptionalPool.Get()
+	a.typeOptionalAllocations = append(a.typeOptionalAllocations, v)
+	return v
 }
 
 func (a *Allocator) BoolValue() (v *Ydb.Value_BoolValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &boolPool.Pool})
-	}()
-	return boolPool.Get()
+	v = boolPool.Get()
+	a.boolAllocations = append(a.boolAllocations, v)
+	return v
 }
 
 func (a *Allocator) BytesValue() (v *Ydb.Value_BytesValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &bytesPool.Pool})
-	}()
-	return bytesPool.Get()
+	v = bytesPool.Get()
+	a.bytesAllocations = append(a.bytesAllocations, v)
+	return v
 }
 
 func (a *Allocator) Int32Value() (v *Ydb.Value_Int32Value) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &int32Pool.Pool})
-	}()
-	return int32Pool.Get()
+	v = int32Pool.Get()
+	a.int32Allocations = append(a.int32Allocations, v)
+	return v
 }
 
 func (a *Allocator) Int64Value() (v *Ydb.Value_Int64Value) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &int64Pool.Pool})
-	}()
-	return int64Pool.Get()
+	v = int64Pool.Get()
+	a.int64Allocations = append(a.int64Allocations, v)
+	return v
 }
 
 func (a *Allocator) Uint32Value() (v *Ydb.Value_Uint32Value) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &uint32Pool.Pool})
-	}()
-	return uint32Pool.Get()
+	v = uint32Pool.Get()
+	a.uint32Allocations = append(a.uint32Allocations, v)
+	return v
 }
 
 func (a *Allocator) FloatValue() (v *Ydb.Value_FloatValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &floatPool.Pool})
-	}()
-	return floatPool.Get()
+	v = floatPool.Get()
+	a.floatAllocations = append(a.floatAllocations, v)
+	return v
 }
 
 func (a *Allocator) DoubleValue() (v *Ydb.Value_DoubleValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &doublePool.Pool})
-	}()
-	return doublePool.Get()
+	v = doublePool.Get()
+	a.doubleAllocations = append(a.doubleAllocations, v)
+	return v
 }
 
 func (a *Allocator) Uint64Value() (v *Ydb.Value_Uint64Value) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &uint64Pool.Pool})
-	}()
-	return uint64Pool.Get()
+	v = uint64Pool.Get()
+	a.uint64Allocations = append(a.uint64Allocations, v)
+	return v
 }
 
 func (a *Allocator) TextValue() (v *Ydb.Value_TextValue) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &textPool.Pool})
-	}()
-	return textPool.Get()
+	v = textPool.Get()
+	a.textAllocations = append(a.textAllocations, v)
+	return v
 }
 
 func (a *Allocator) Low128Value() (v *Ydb.Value_Low_128) {
-	defer func() {
-		a.allocations = append(a.allocations, free{v, &low128Pool.Pool})
-	}()
-	return low128Pool.Get()
+	v = low128Pool.Get()
+	a.low128Allocations = append(a.low128Allocations, v)
+	return v
 }
--- internal/value/exp/value_test.go
+++ internal/value/exp/value_test.go
@@ -10,41 +10,46 @@ func BenchmarkMemory(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		func() {
 			a := allocator.New()
-			defer a.Close()
-			_ = typedValue(TupleValue(
-				StringValue([]byte("test")),
+			defer a.Free()
+			params := []V{
 				BoolValue(true),
-				DateValue(1),
-				DatetimeValue(1),
-				DecimalValue([...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6}, 22, 9),
-				DoubleValue(1),
-				DyNumberValue("123"),
-				FloatValue(1),
 				Int8Value(1),
 				Int16Value(1),
 				Int32Value(1),
 				Int64Value(1),
-				IntervalValue(1),
-				JSONValue("{}"),
-				JSONDocumentValue("{}"),
-				TimestampValue(1),
-				TzDateValue("1"),
-				TzDatetimeValue("1"),
-				TzTimestampValue("1"),
 				Uint8Value(1),
 				Uint16Value(1),
 				Uint32Value(1),
 				Uint64Value(1),
-				UTF8Value("1"),
-				UUIDValue([...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6}),
+				DateValue(1),
+				DatetimeValue(1),
+				TimestampValue(1),
+				IntervalValue(1),
 				VoidValue(),
-				YSONValue("{}"),
-				ListValue(
-					Int64Value(1),
-					Int64Value(2),
-					Int64Value(3),
-				),
-			), a)
+
+				//FloatValue(1),
+				//DoubleValue(1),
+				//StringValue([]byte("test")),
+				//DecimalValue([...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6}, 22, 9),
+				//DyNumberValue("123"),
+				//JSONValue("{}"),
+				//JSONDocumentValue("{}"),
+				//TzDateValue("1"),
+				//TzDatetimeValue("1"),
+				//TzTimestampValue("1"),
+				//UTF8Value("1"),
+				//UUIDValue([...]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6}),
+				//YSONValue("{}"),
+				//ListValue(
+				//	Int64Value(1),
+				//	Int64Value(2),
+				//	Int64Value(3),
+				//),
+			}
+			for _, p := range params {
+				_ = typedValue(p, a)
+			}
+			//_ = typedValue(TupleValue(params...), a)
 		}()
 	}
 }
