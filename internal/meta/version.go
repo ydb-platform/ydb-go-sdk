62b2a47fdcb455ea4e5fd516ffa926526c0e2071
robot <robot@umbrella>
2022-08-26 21:34:13 +0000

    Release v3.37.0

--- internal/meta/version.go
+++ internal/meta/version.go
@@ -2,8 +2,8 @@ package meta
 
 const (
 	VersionMajor = "3"
-	VersionMinor = "36"
-	VersionPatch = "2"
+	VersionMinor = "37"
+	VersionPatch = "0"
 )
 
 const Version = VersionMajor + "." + VersionMinor + "." + VersionPatch
