14bfe7027d6a3be246644c8b030a721fe27814ac
asmyasnikov <79263256394@ya.ru>
2022-09-20 14:30:04 +0300

    added tests for check deadlocks in updateNodes and internalPoolGC

--- internal/table/client.go
+++ internal/table/client.go
@@ -701,6 +701,32 @@ func (c *Client) DoTx(ctx context.Context, op table.TxOperation, opts ...table.O
 	return nil
 }
 
+func (c *Client) internalPoolGCTick(ctx context.Context, idleThreshold time.Duration) {
+	c.mu.WithLock(func() {
+		if c.isClosed() {
+			return
+		}
+		for e := c.idle.Front(); e != nil; e = e.Next() {
+			s := e.Value.(*session)
+			info, has := c.index[s]
+			if !has {
+				panic("session not found in pool")
+			}
+			if info.idle == nil {
+				panic("inconsistent session info")
+			}
+			if since := timeutil.Until(info.touched); since > idleThreshold {
+				s.SetStatus(table.SessionClosing)
+				c.wg.Add(1)
+				go func() {
+					defer c.wg.Done()
+					c.internalPoolSyncCloseSession(ctx, s)
+				}()
+			}
+		}
+	})
+}
+
 func (c *Client) internalPoolGC(ctx context.Context, idleThreshold time.Duration) {
 	defer c.wg.Done()
 
@@ -712,31 +738,12 @@ func (c *Client) internalPoolGC(ctx context.Context, idleThreshold time.Duration
 		case <-c.done:
 			return
 
+		case <-ctx.Done():
+			return
+
 		case <-timer.C():
-			c.mu.WithLock(func() {
-				if c.isClosed() {
-					return
-				}
-				for e := c.idle.Front(); e != nil; e = e.Next() {
-					s := e.Value.(*session)
-					info, has := c.index[s]
-					if !has {
-						panic("session not found in pool")
-					}
-					if info.idle == nil {
-						panic("inconsistent session info")
-					}
-					if since := timeutil.Until(info.touched); since > idleThreshold {
-						s.SetStatus(table.SessionClosing)
-						c.wg.Add(1)
-						go func() {
-							defer c.wg.Done()
-							c.internalPoolSyncCloseSession(ctx, s)
-						}()
-					}
-				}
-				timer.Reset(idleThreshold / 2)
-			})
+			c.internalPoolGCTick(ctx, idleThreshold)
+			timer.Reset(idleThreshold / 2)
 		}
 	}
 }
--- internal/table/client_test.go
+++ internal/table/client_test.go
@@ -11,11 +11,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
 	"github.com/ydb-platform/ydb-go-genproto/protos/Ydb_Table"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 
+	"github.com/ydb-platform/ydb-go-sdk/v3/internal/endpoint"
 	"github.com/ydb-platform/ydb-go-sdk/v3/internal/table/config"
 	"github.com/ydb-platform/ydb-go-sdk/v3/internal/xerrors"
 	"github.com/ydb-platform/ydb-go-sdk/v3/internal/xrand"
@@ -938,4 +940,78 @@ func whenWantWaitCh(p *Client) <-chan struct{} {
 		close(ch)
 	}
 	return ch
+}
+
+func TestDeadlockOnUpdateNodes(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	nodes := make([]uint32, 0, 3)
+	balancer := testutil.NewBalancer(testutil.WithInvokeHandlers(testutil.InvokeHandlers{
+		testutil.TableCreateSession: func(interface{}) (proto.Message, error) {
+			sessionID := testutil.SessionID()
+			nodeID, err := nodeID(sessionID)
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, nodeID)
+			return &Ydb_Table.CreateSessionResult{
+				SessionId: sessionID,
+			}, nil
+		},
+	}))
+	c := newClientWithStubBuilder(t, balancer, 3)
+	defer func() {
+		_ = c.Close(ctx)
+	}()
+	s1, err := c.Get(ctx)
+	require.NoError(t, err)
+	s2, err := c.Get(ctx)
+	require.NoError(t, err)
+	s3, err := c.Get(ctx)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(nodes))
+	err = c.Put(ctx, s1)
+	require.NoError(t, err)
+	err = c.Put(ctx, s2)
+	require.NoError(t, err)
+	err = c.Put(ctx, s3)
+	require.NoError(t, err)
+	c.updateNodes(ctx, []endpoint.Info{})
+}
+
+func TestDeadlockOnInternalPoolGCTick(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	nodes := make([]uint32, 0, 3)
+	balancer := testutil.NewBalancer(testutil.WithInvokeHandlers(testutil.InvokeHandlers{
+		testutil.TableCreateSession: func(interface{}) (proto.Message, error) {
+			sessionID := testutil.SessionID()
+			nodeID, err := nodeID(sessionID)
+			if err != nil {
+				return nil, err
+			}
+			nodes = append(nodes, nodeID)
+			return &Ydb_Table.CreateSessionResult{
+				SessionId: sessionID,
+			}, nil
+		},
+	}))
+	c := newClientWithStubBuilder(t, balancer, 3)
+	defer func() {
+		_ = c.Close(ctx)
+	}()
+	s1, err := c.Get(ctx)
+	require.NoError(t, err)
+	s2, err := c.Get(ctx)
+	require.NoError(t, err)
+	s3, err := c.Get(ctx)
+	require.NoError(t, err)
+	require.Equal(t, 3, len(nodes))
+	err = c.Put(ctx, s1)
+	require.NoError(t, err)
+	err = c.Put(ctx, s2)
+	require.NoError(t, err)
+	err = c.Put(ctx, s3)
+	require.NoError(t, err)
+	c.internalPoolGCTick(ctx, 0)
 }
