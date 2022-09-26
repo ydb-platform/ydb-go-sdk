319a62ca01d7179b4c8d5c4a66a268e2e125535d
Aleksandr Petrukhin <shmel1k@yandex-team.ru>
2022-09-26 14:43:54 +0300

    [topic] add missing RetentionPeriod on describe

--- topic/topictypes/topictypes.go
+++ topic/topictypes/topictypes.go
@@ -193,6 +193,9 @@ func (d *TopicDescription) FromRaw(raw *rawtopic.DescribeTopicResult) {
 	d.PartitionWriteSpeedBytesPerSecond = raw.PartitionWriteSpeedBytesPerSecond
 	d.PartitionWriteBurstBytes = raw.PartitionWriteBurstBytes
 
+	d.RetentionPeriod = raw.RetentionPeriod
+	d.RetentionStorageMB = raw.RetentionStorageMB
+
 	d.Attributes = make(map[string]string)
 	for k, v := range raw.Attributes {
 		d.Attributes[k] = v
