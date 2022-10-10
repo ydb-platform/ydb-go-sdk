8364299d0a2bac5099a2481c0262f9c94f64ec05
Timofey Koolin <timofey.koolin@gmail.com>
2022-10-10 09:52:37 +0300

    change many tests from fixed test number to time limit

--- internal/xsync/mutex_test.go
+++ internal/xsync/mutex_test.go
@@ -7,10 +7,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3/internal/xtest"
 )
 
 func TestMutex(t *testing.T) {
-	for i := 0; i < 100; i++ {
+	xtest.TestManyTimes(t, func(t testing.TB) {
 		var m Mutex
 		a, b := 1, 1
 
@@ -32,73 +34,71 @@ func TestMutex(t *testing.T) {
 		wg.Wait()
 		require.Equal(t, 2, a)
 		require.Equal(t, 2, b)
-	}
+	})
 }
 
 func TestRWMutex(t *testing.T) {
-	for testIteration := 0; testIteration < 100; testIteration++ {
-		t.Run("WithLock", func(t *testing.T) {
-			var m Mutex
-			a, b := 1, 1
+	xtest.TestManyTimesWithName(t, "WithLock", func(t testing.TB) {
+		var m Mutex
+		a, b := 1, 1
 
-			var wg sync.WaitGroup
-			f := func() {
-				defer wg.Done()
+		var wg sync.WaitGroup
+		f := func() {
+			defer wg.Done()
 
-				if a+b == 2 {
-					a = 2
-				} else {
-					b = 2
-				}
+			if a+b == 2 {
+				a = 2
+			} else {
+				b = 2
 			}
+		}
 
-			wg.Add(2)
-			go m.WithLock(f)
-			go m.WithLock(f)
+		wg.Add(2)
+		go m.WithLock(f)
+		go m.WithLock(f)
 
-			wg.Wait()
-			require.Equal(t, 2, a)
-			require.Equal(t, 2, b)
-		})
-		t.Run("WithRLock", func(t *testing.T) {
-			var m RWMutex
-			a, b := 1, 1
+		wg.Wait()
+		require.Equal(t, 2, a)
+		require.Equal(t, 2, b)
+	})
+	xtest.TestManyTimesWithName(t, "WithRLock", func(t testing.TB) {
+		var m RWMutex
+		a, b := 1, 1
 
-			var badSummCount int64
-			var wg sync.WaitGroup
+		var badSummCount int64
+		var wg sync.WaitGroup
 
-			for reader := 0; reader < 100; reader++ {
-				wg.Add(1)
-				go func() {
-					defer wg.Done()
-
-					for i := 0; i < 1000; i++ {
-						m.WithRLock(func() {
-							if a+b != 2 {
-								atomic.AddInt64(&badSummCount, 1)
-							}
-						})
-						runtime.Gosched()
-					}
-				}()
-			}
-
+		for reader := 0; reader < 100; reader++ {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
 
-				for i := 0; i < 100; i++ {
-					m.WithLock(func() {
-						a++
-						b--
+				for i := 0; i < 1000; i++ {
+					m.WithRLock(func() {
+						if a+b != 2 {
+							atomic.AddInt64(&badSummCount, 1)
+						}
 					})
 					runtime.Gosched()
 				}
 			}()
+		}
 
-			wg.Wait()
-			require.Equal(t, 2, a+b)
-			require.Equal(t, int64(0), badSummCount)
-		})
-	}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			for i := 0; i < 100; i++ {
+				m.WithLock(func() {
+					a++
+					b--
+				})
+				runtime.Gosched()
+			}
+		}()
+
+		wg.Wait()
+		require.Equal(t, 2, a+b)
+		require.Equal(t, int64(0), badSummCount)
+	})
 }
