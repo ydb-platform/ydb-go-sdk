82287d95b9b8c3669d6fea2ef17da794e53c0561
asmyasnikov <79263256394@ya.ru>
2022-10-14 12:05:37 +0300

    WIP

--- table/table_e2e_test.go
+++ table/table_e2e_test.go
@@ -7,12 +7,10 @@ import (
 	"bytes"
 	"context"
 	"crypto/sha256"
-	"database/sql"
 	"encoding/binary"
 	"fmt"
 	"github.com/stretchr/testify/require"
-	"github.com/ydb-platform/ydb-go-sdk/v3/internal/value"
-	"github.com/ydb-platform/ydb-go-sdk/v3/retry"
+	"golang.org/x/xerrors"
 	"math"
 	"net/http"
 	"os"
@@ -1781,99 +1779,184 @@ func TestSplitRangesAndRead(t *testing.T) {
 	})
 }
 
-func TestTypeToString(t *testing.T) {
-	db, err := sql.Open("ydb", os.Getenv("YDB_CONNECTION_STRING"))
-	if err != nil {
-		t.Fatal(err)
-	}
+type issue229Struct struct{}
 
-	for _, tt := range []value.Type{
-		value.Void(),
-		value.Null(),
-		value.TypeBool,
-		value.TypeInt8,
-		value.TypeUint8,
-		value.TypeInt16,
-		value.TypeUint16,
-		value.TypeInt32,
-		value.TypeUint32,
-		value.TypeInt64,
-		value.TypeUint64,
-		value.TypeFloat,
-		value.TypeDouble,
-		value.TypeDate,
-		value.TypeDatetime,
-		value.TypeTimestamp,
-		value.TypeInterval,
-		value.TypeTzDate,
-		value.TypeTzDatetime,
-		value.TypeTzTimestamp,
-		value.TypeBytes,
-		value.TypeText,
-		value.TypeYSON,
-		value.TypeJSON,
-		value.TypeUUID,
-		value.TypeJSONDocument,
-		value.TypeDyNumber,
-		value.Optional(value.TypeBool),
-		value.Optional(value.TypeInt8),
-		value.Optional(value.TypeUint8),
-		value.Optional(value.TypeInt16),
-		value.Optional(value.TypeUint16),
-		value.Optional(value.TypeInt32),
-		value.Optional(value.TypeUint32),
-		value.Optional(value.TypeInt64),
-		value.Optional(value.TypeUint64),
-		value.Optional(value.TypeFloat),
-		value.Optional(value.TypeDouble),
-		value.Optional(value.TypeDate),
-		value.Optional(value.TypeDatetime),
-		value.Optional(value.TypeTimestamp),
-		value.Optional(value.TypeInterval),
-		value.Optional(value.TypeTzDate),
-		value.Optional(value.TypeTzDatetime),
-		value.Optional(value.TypeTzTimestamp),
-		value.Optional(value.TypeBytes),
-		value.Optional(value.TypeText),
-		value.Optional(value.TypeYSON),
-		value.Optional(value.TypeJSON),
-		value.Optional(value.TypeUUID),
-		value.Optional(value.TypeJSONDocument),
-		value.Optional(value.TypeDyNumber),
-		value.Decimal(22, 9),
-		value.Dict(value.TypeText, value.TypeTimestamp),
-		value.EmptyList(),
-		value.List(value.TypeUint32),
-		value.Variant(value.Struct(
-			value.StructField{
-				Name: "a",
-				T:    value.TypeBool,
-			},
-			value.StructField{
-				Name: "b",
-				T:    value.TypeFloat,
-			},
-		)),
-		value.Variant(value.Tuple(
-			value.TypeBool,
-			value.TypeFloat,
-		)),
-	} {
-		t.Run(tt.String(), func(t *testing.T) {
-			var got string
-			err := retry.Do(context.Background(), db, func(ctx context.Context, cc *sql.Conn) error {
-				row := cc.QueryRowContext(ydb.WithQueryMode(ctx, ydb.ScriptingQueryMode),
-					fmt.Sprintf("SELECT FormatType(ParseType(\"%s\"))", tt.String()),
-				)
-				if err := row.Scan(&got); err != nil {
-					return err
-				}
-				return row.Err()
-			})
-			require.NoError(t, err)
-			if got != tt.String() {
-				t.Errorf("s representations not equals:\n\n -  got: %s\n\n - want: %s", got, tt.String())
+// UnmarshalJSON implements json.Unmarshaler
+func (i *issue229Struct) UnmarshalJSON(_ []byte) error {
+	return nil
+}
+
+func TestIssue229UnexpectedNullWhileParseNilJsonDocumentValue(t *testing.T) {
+	// https://github.com/ydb-platform/ydb-go-sdk/issues/229
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db := connect(t)
+	defer db.Close(ctx)
+	err := db.Table().DoTx(ctx, func(ctx context.Context, tx table.TransactionActor) error {
+		res, err := tx.Execute(ctx, `SELECT Nothing(JsonDocument?) AS r`, nil)
+		require.NoError(t, err)
+		require.NoError(t, res.NextResultSetErr(ctx))
+		require.True(t, res.NextRow())
+
+		var val issue229Struct
+		require.NoError(t, res.Scan(&val))
+		return nil
+	})
+	require.NoError(t, err)
+}
+
+func connect(t testing.TB) ydb.Connection {
+	db, err := ydb.Open(
+		context.Background(),
+		os.Getenv("YDB_CONNECTION_STRING"),
+		ydb.WithAccessTokenCredentials(os.Getenv("YDB_ACCESS_TOKEN_CREDENTIALS")))
+	require.NoError(t, err)
+	return db
+}
+
+func TestIssue259IntervalFromDuration(t *testing.T) {
+	// https://github.com/ydb-platform/ydb-go-sdk/issues/259
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db := connect(t)
+	defer db.Close(ctx)
+	err := db.Table().DoTx(ctx, func(ctx context.Context, tx table.TransactionActor) error {
+		// Check about interval work with microseconds
+		res, err := tx.Execute(ctx, `DECLARE $ts as Interval;
+			$ten_micro = CAST(10 as Interval);
+			SELECT $ts == $ten_micro, $ten_micro;`, table.NewQueryParameters(
+			table.ValueParam(`$ts`, types.IntervalValueFromDuration(10*time.Microsecond)),
+		))
+		require.NoError(t, err)
+		require.NoError(t, res.NextResultSetErr(ctx))
+		require.True(t, res.NextRow())
+
+		var (
+			valuesEqual bool
+			tenMicro    time.Duration
+		)
+		require.NoError(t, res.Scan(&valuesEqual, &tenMicro))
+		require.True(t, valuesEqual)
+		require.Equal(t, 10*time.Microsecond, tenMicro)
+
+		// Check about parse interval represent date interval
+		query := `
+		SELECT 
+			DateTime::MakeTimestamp(DateTime::ParseIso8601("2009-02-14T02:31:30+0000")) - 
+			DateTime::MakeTimestamp(DateTime::ParseIso8601("2009-02-14T01:31:30+0000")) 
+		`
+		res, err = tx.Execute(ctx, query, nil)
+		require.NoError(t, err)
+		require.NoError(t, res.NextResultSetErr(ctx))
+		require.True(t, res.NextRow())
+
+		var delta time.Duration
+		require.NoError(t, res.ScanWithDefaults(&delta))
+		require.Equal(t, time.Hour, delta)
+
+		// check about send interval work find with dates
+		query = `
+		DECLARE $delta AS Interval;
+	
+		SELECT 
+			DateTime::MakeTimestamp(DateTime::ParseIso8601("2009-02-14T01:31:30+0000")) + $delta ==
+			DateTime::MakeTimestamp(DateTime::ParseIso8601("2009-02-14T02:31:30+0000"))
+		`
+		res, err = tx.Execute(ctx, query, table.NewQueryParameters(
+			table.ValueParam("$delta", types.IntervalValueFromDuration(time.Hour))),
+		)
+		require.NoError(t, err)
+		require.NoError(t, res.NextResultSetErr(ctx))
+		require.True(t, res.NextRow())
+
+		require.NoError(t, res.ScanWithDefaults(&valuesEqual))
+		require.True(t, valuesEqual)
+
+		return nil
+	})
+	require.NoError(t, err)
+}
+
+func TestIssue415ScanError(t *testing.T) {
+	// https://github.com/ydb-platform/ydb-go-sdk/issues/415
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db := connect(t)
+	defer db.Close(ctx)
+	err := db.Table().DoTx(ctx, func(ctx context.Context, tx table.TransactionActor) error {
+		res, err := tx.Execute(ctx, `SELECT 1 as abc, 2 as def;`, nil)
+		if err != nil {
+			return err
+		}
+		err = res.NextResultSetErr(ctx)
+		if err != nil {
+			return err
+		}
+		if !res.NextRow() {
+			if err = res.Err(); err != nil {
+				return err
 			}
-		})
-	}
+			return fmt.Errorf("unexpected empty result set")
+		}
+		var abc, def int32
+		err = res.ScanNamed(
+			named.Required("abc", &abc),
+			named.Required("ghi", &def),
+		)
+		if err != nil {
+			return err
+		}
+		fmt.Println(abc, def)
+		return res.Err()
+	}, table.WithTxSettings(table.TxSettings(table.WithSnapshotReadOnly())))
+	require.Error(t, err)
+	err = func(err error) error {
+		for {
+			//nolint:errorlint
+			if unwrappedErr, has := err.(xerrors.Wrapper); has {
+				err = unwrappedErr.Unwrap()
+			} else {
+				return err
+			}
+		}
+	}(err)
+	require.Equal(t, "not found column 'ghi'", err.Error())
+}
+
+func TestNullType(t *testing.T) {
+	// https://github.com/ydb-platform/ydb-go-sdk/issues/415
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db := connect(t)
+	defer db.Close(ctx)
+	err := db.Table().DoTx(ctx, func(ctx context.Context, tx table.TransactionActor) error {
+		res, err := tx.Execute(ctx, `SELECT NULL AS reschedule_due;`, nil)
+		if err != nil {
+			return err
+		}
+		err = res.NextResultSetErr(ctx)
+		if err != nil {
+			return err
+		}
+		if !res.NextRow() {
+			if err = res.Err(); err != nil {
+				return err
+			}
+			return fmt.Errorf("unexpected empty result set")
+		}
+		var rescheduleDue *time.Time
+		err = res.ScanNamed(
+			named.Optional("reschedule_due", &rescheduleDue),
+		)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%+v\n", rescheduleDue)
+		return res.Err()
+	}, table.WithTxSettings(table.TxSettings(table.WithSnapshotReadOnly())))
+	require.NoError(t, err)
 }
