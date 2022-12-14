12890c5fe57f6e126a5b72c41544431d2c3955d5
asmyasnikov <79263256394@ya.ru>
2022-12-15 02:40:55 +0300

    switch ydb docker image for tests

--- .github/scripts/wait-ydb-container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while ! docker run --network host cr.yandex/yc/yandex-docker-local-ydb:latest /ydb -e grpc://localhost:2136 -d /local scheme ls; do
-  echo wait db...
-  sleep 3
-done
-echo DB available
