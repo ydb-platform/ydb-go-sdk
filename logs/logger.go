5847224e8f9f9d43066d05003af3ec452d76b61b
Арсений Хорошилов <horoshilov.aa@phystech.edu>
2022-12-26 13:13:31 +0600

    add Logger.Enabled method

--- logs/logger.go
+++ logs/logger.go
@@ -2,6 +2,13 @@ package logs
 
 type Logger interface {
 	Log(opts Options, msg string, fields ...Field)
+
+	// Enabled tells whether the loggin level is enabled by the logger.
+	// It can be used for optimization to prevent expensive field initialization
+	// for disabled messages. If the Logger does not support this feature,
+	// its Enabled() method should always return true.
+	// Logger should expect calls to Log() even if it is disabled.
+	Enabled(lvl Level) bool
 }
 
 type Options struct {
