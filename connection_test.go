abe506959c4b20fc7690893a4ef4cecf5ba28870
asmyasnikov <79263256394@ya.ru>
2023-01-28 13:31:28 +0300

    add test for check many attempts on cluster discovery step

--- connection_test.go
+++ connection_test.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/ydb-platform/ydb-go-sdk/v3/trace"
 	"testing"
 	"time"
 
@@ -60,4 +61,27 @@ func TestZeroDialTimeout(t *testing.T) {
 	require.Error(t, err)
 	require.Nil(t, db)
 	require.True(t, errors.Is(err, context.DeadlineExceeded) || ydb.IsTransportError(err, grpcCodes.DeadlineExceeded))
+}
+
+func TestClusterDiscoveryRetry(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	counter := 0
+
+	db, err := ydb.Open(ctx,
+		"grpc://non-existent.com:2135/some",
+		ydb.WithDialTimeout(time.Second),
+		ydb.WithTraceDriver(trace.Driver{
+			OnBalancerUpdate: func(info trace.DriverBalancerUpdateStartInfo) func(trace.DriverBalancerUpdateDoneInfo) {
+				counter++
+				return nil
+			},
+		}),
+	)
+	t.Logf("attempts: %d", counter)
+	require.Error(t, err)
+	require.Nil(t, db)
+	require.True(t, errors.Is(err, context.DeadlineExceeded) || ydb.IsTransportError(err, grpcCodes.DeadlineExceeded))
+	require.Greater(t, counter, 1)
 }
