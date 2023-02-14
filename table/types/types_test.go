0161a14fb9626a25c79fdef349b56e540baed334
Aleksey Myasnikov <79263256394.ya@gmail.com>
2023-02-14 20:15:45 +0300

    Added internal methods for optional type for casts some type to optional

--- table/types/types_test.go
+++ table/types/types_test.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestEqual(t *testing.T) {
@@ -45,6 +47,58 @@ func TestEqual(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			if equal := Equal(tt.lhs, tt.rhs); equal != tt.equal {
 				t.Errorf("Equal(%s, %s) = %v, want %v", tt.lhs, tt.rhs, equal, tt.equal)
+			}
+		})
+	}
+}
+
+func TestOptionalInnerType(t *testing.T) {
+	tests := []struct {
+		src        Type
+		innerType  Type
+		isOptional bool
+	}{
+		{
+			TypeBool,
+			nil,
+			false,
+		},
+		{
+			TypeText,
+			nil,
+			false,
+		},
+		{
+			Optional(TypeBool),
+			TypeBool,
+			true,
+		},
+		{
+			Optional(TypeText),
+			TypeText,
+			true,
+		},
+		{
+			Optional(Tuple(TypeText, TypeBool, TypeUint64, Optional(TypeInt64))),
+			Tuple(TypeText, TypeBool, TypeUint64, Optional(TypeInt64)),
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			optional, isOptional := tt.src.(interface {
+				IsOptional()
+				InnerType() Type
+			})
+			require.Equal(t, tt.isOptional, isOptional)
+			var innerType Type
+			if isOptional {
+				innerType = optional.InnerType()
+			}
+			if tt.innerType == nil {
+				require.Nil(t, innerType)
+			} else {
+				require.True(t, Equal(tt.innerType, innerType))
 			}
 		})
 	}
