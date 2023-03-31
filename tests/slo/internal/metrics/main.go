79552530b8fdf9e44714f95c99bb36185023441e
Vadim Vladimirov <impressionableracoon@gmail.com>
2023-03-31 18:02:30 +0300

    update: latencies: use reset instead of observe(0)

--- tests/slo/internal/metrics/main.go
+++ tests/slo/internal/metrics/main.go
@@ -90,19 +90,16 @@ func (m *Metrics) Push() error {
 }
 
 func (m *Metrics) Reset() error {
-	m.oks.WithLabelValues("read").Set(0)
-	m.oks.WithLabelValues("write").Set(0)
+	m.oks.WithLabelValues(JobRead).Set(0)
+	m.oks.WithLabelValues(JobWrite).Set(0)
 
-	m.notOks.WithLabelValues("read").Set(0)
-	m.notOks.WithLabelValues("write").Set(0)
+	m.notOks.WithLabelValues(JobRead).Set(0)
+	m.notOks.WithLabelValues(JobWrite).Set(0)
 
-	m.inflight.WithLabelValues("read").Set(0)
-	m.inflight.WithLabelValues("write").Set(0)
+	m.inflight.WithLabelValues(JobRead).Set(0)
+	m.inflight.WithLabelValues(JobWrite).Set(0)
 
-	m.latencies.WithLabelValues("ok", "read").Observe(0)
-	m.latencies.WithLabelValues("ok", "write").Observe(0)
-	m.latencies.WithLabelValues("err", "read").Observe(0)
-	m.latencies.WithLabelValues("err", "write").Observe(0)
+	m.latencies.Reset()
 
 	return m.Push()
 }
