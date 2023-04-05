9583475b367676a90a163adf18984445e6178ed8
Vadim Vladimirov <impressionableracoon@gmail.com>
2023-04-05 18:01:21 +0300

    feat: use only slo local for workload

--- .github/scripts/format-go-code.sh
+++ .github/scripts/format-go-code.sh
@@ -7,7 +7,13 @@ FILEPATH="$1"
 gofmt -s -w "$FILEPATH"
 
 # https://github.com/rinchsan/gosimports
-gosimports -local github.com/ydb-platform/ydb-go-sdk/v3,slo -w "$FILEPATH"
+if [[ "$FILEPATH" == *"tests/slo/"* ]]
+then
+  gosimports -local slo -w "$FILEPATH"
+else
+  gosimports -local github.com/ydb-platform/ydb-go-sdk/v3 -w "$FILEPATH"
+fi
+
 
 # https://github.com/mvdan/gofumpt
 gofumpt -w "$FILEPATH"
