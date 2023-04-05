0ff907c812c38c4d3c34a787e1228e249a622811
Vadim Vladimirov <impressionableracoon@gmail.com>
2023-04-05 15:42:45 +0300

    feat: add slo support to linter

--- .github/scripts/format-go-code.sh
+++ .github/scripts/format-go-code.sh
@@ -7,7 +7,7 @@ FILEPATH="$1"
 gofmt -s -w "$FILEPATH"
 
 # https://github.com/rinchsan/gosimports
-gosimports -local github.com/ydb-platform/ydb-go-sdk/v3 -w "$FILEPATH"
+gosimports -local github.com/ydb-platform/ydb-go-sdk/v3,slo -w "$FILEPATH"
 
 # https://github.com/mvdan/gofumpt
 gofumpt -w "$FILEPATH"
