6a7eb62871270f012b2137e908cca2e0b9166749
Vadim Vladimirov <impressionableracoon@gmail.com>
2023-04-07 15:55:51 +0300

    fix: shift args 1 left

--- tests/slo/internal/config/config.go
+++ tests/slo/internal/config/config.go
@@ -35,16 +35,16 @@ type Config struct {
 }
 
 func New() (cfg Config, err error) {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 2 {
 		fmt.Print(mainHelp)
 		return cfg, ErrWrongArgs
 	}
 
 	fs := flag.FlagSet{}
 
-	switch os.Args[2] {
+	switch os.Args[1] {
 	case "create":
-		if len(os.Args) < 5 {
+		if len(os.Args) < 4 {
 			fmt.Print(createHelp)
 			return cfg, ErrWrongArgs
 		}
@@ -56,14 +56,14 @@ func New() (cfg Config, err error) {
 
 		cfg.Mode = CreateMode
 	case "cleanup":
-		if len(os.Args) < 5 {
+		if len(os.Args) < 4 {
 			fmt.Print(cleanupHelp)
 			return cfg, ErrWrongArgs
 		}
 
 		cfg.Mode = CleanupMode
 	case "run":
-		if len(os.Args) < 5 {
+		if len(os.Args) < 4 {
 			fmt.Print(runHelp)
 			return cfg, ErrWrongArgs
 		}
@@ -86,13 +86,13 @@ func New() (cfg Config, err error) {
 		return cfg, ErrWrongArgs
 	}
 
-	cfg.Endpoint = os.Args[3]
-	cfg.DB = os.Args[4]
+	cfg.Endpoint = os.Args[2]
+	cfg.DB = os.Args[3]
 
 	fs.StringVar(&cfg.YDBToken, "a", "", "YDB access token credentials")
 	fs.StringVar(&cfg.Table, "t", "testingTable", "table name")
 
-	err = fs.Parse(os.Args[5:])
+	err = fs.Parse(os.Args[4:])
 	if err != nil {
 		return cfg, err
 	}
