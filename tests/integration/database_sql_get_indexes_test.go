e34276701cc691a31f267ca76d494c2643d846dc
Timofey Koolin <timofey.koolin@gmail.com>
2023-07-23 14:53:22 +0300

    fix typos

--- tests/integration/database_sql_get_indexes_test.go
+++ tests/integration/database_sql_get_indexes_test.go
@@ -22,10 +22,10 @@ func TestDatabaseSqlGetIndexes(t *testing.T) {
 	)
 
 	// create table
-		err := retry.Do(scope.Ctx, db, func(ctx context.Context, cc *sql.Conn) (err error) {
-			_, err = cc.ExecContext(
-				ydb.WithQueryMode(ctx, ydb.SchemeQueryMode),
-				`CREATE TABLE seasons (
+	err := retry.Do(scope.Ctx, db, func(ctx context.Context, cc *sql.Conn) (err error) {
+		_, err = cc.ExecContext(
+			ydb.WithQueryMode(ctx, ydb.SchemeQueryMode),
+			`CREATE TABLE seasons (
 				series_id Uint64,
 				season_id Uint64,
 				title UTF8,
@@ -38,44 +38,41 @@ func TestDatabaseSqlGetIndexes(t *testing.T) {
 					season_id
 				)
 			)`,
-			)
+		)
 
-			if err != nil {
-				return err
-			}
+		if err != nil {
+			return err
+		}
 
-			return nil
-		}, retry.WithDoRetryOptions(retry.WithIdempotent(true)))
+		return nil
+	}, retry.WithDoRetryOptions(retry.WithIdempotent(true)))
 
-		require.NoError(t, err)
-	})
+	require.NoError(t, err)
 
-	t.Run("get-indexes", func(t *testing.T) {
-		err := retry.Do(scope.Ctx, db, func(ctx context.Context, cc *sql.Conn) (err error) {
-			indexes := make([]string, 0)
-			err = cc.Raw(func(drvConn interface{}) (err error) {
-				q, ok := drvConn.(interface {
-					GetIndexes(context.Context, string) ([]string, error)
-				})
-
-				if !ok {
-					return errors.New("drvConn does not implement extended API")
-				}
-
-				indexes, err = q.GetIndexes(ctx, "./seasons")
-				return err
+	err = retry.Do(scope.Ctx, db, func(ctx context.Context, cc *sql.Conn) (err error) {
+		indexes := make([]string, 0)
+		err = cc.Raw(func(drvConn interface{}) (err error) {
+			q, ok := drvConn.(interface {
+				GetIndexes(context.Context, string) ([]string, error)
 			})
 
-			if err != nil {
-				return err
+			if !ok {
+				return errors.New("drvConn does not implement extended API")
 			}
 
-			require.ElementsMatch(t,
-				[]string{"index_series_title", "index_seasons_aired_date"},
-				indexes)
-			return nil
-		}, retry.WithDoRetryOptions(retry.WithIdempotent(true)))
+			indexes, err = q.GetIndexes(ctx, "./seasons")
+			return err
+		})
 
-		require.NoError(t, err)
-	})
+		if err != nil {
+			return err
+		}
+
+		require.ElementsMatch(t,
+			[]string{"index_series_title", "index_seasons_aired_date"},
+			indexes)
+		return nil
+	}, retry.WithDoRetryOptions(retry.WithIdempotent(true)))
+
+	require.NoError(t, err)
 }
