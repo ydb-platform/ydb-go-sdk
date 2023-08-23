7656182a32043691411a74603c66ce9a7cab968d
Timofey Koolin <timofey.koolin@gmail.com>
2023-08-23 11:41:29 +0300

    add tests

--- internal/conn/error_test.go
+++ internal/conn/error_test.go
@@ -22,3 +22,41 @@ func TestNodeErrorUnwrap(t *testing.T) {
 	unwrapped := errors.Unwrap(nodeErr)
 	require.Equal(t, testErr, unwrapped)
 }
+
+func TestNodeErrorIs(t *testing.T) {
+	testErr := errors.New("test")
+	testErr2 := errors.New("test2")
+	nodeErr := newNodeError(1, "localhost:1234", testErr)
+
+	require.True(t, errors.Is(nodeErr, testErr))
+	require.False(t, errors.Is(nodeErr, testErr2))
+}
+
+type testErrorType1 struct {
+	msg string
+}
+
+func (t testErrorType1) Error() string {
+	return "1 - " + t.msg
+}
+
+type testErrorType2 struct {
+	msg string
+}
+
+func (t testErrorType2) Error() string {
+	return "2 - " + t.msg
+}
+
+func TestNodeErrorAs(t *testing.T) {
+	testErr := testErrorType1{msg: "test"}
+	nodeErr := newNodeError(1, "localhost:1234", testErr)
+
+	target := testErrorType1{}
+	require.True(t, errors.As(nodeErr, &target))
+	require.ErrorAs(t, nodeErr, &target)
+	require.Equal(t, testErr, target)
+
+	target2 := testErrorType2{}
+	require.False(t, errors.As(nodeErr, &target2))
+}
