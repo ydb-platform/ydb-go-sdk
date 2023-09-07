dcc97e097ecbab07477e05ce80ea5c0fc2992d13
Aleksey Myasnikov <79263256394@ya.ru>
2023-09-07 14:44:48 +0300

    Update convert_bench_test.go

--- internal/xstring/convert_bench_test.go
+++ internal/xstring/convert_bench_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Test the performance of the standard conversion string()
-func Benchmark_NormalFromBytes(b *testing.B) {
+func Benchmark_StdFromBytes(b *testing.B) {
 	x := []byte("Hello world! Hello world! Hello world!")
 	for i := 0; i < b.N; i++ {
 		_ = string(x)
@@ -22,7 +22,7 @@ func Benchmark_FromBytes(b *testing.B) {
 }
 
 // Test the performance of standard conversion []byte
-func Benchmark_NormalToBytes(b *testing.B) {
+func Benchmark_StdToBytes(b *testing.B) {
 	x := "Hello world! Hello world! Hello world!"
 	for i := 0; i < b.N; i++ {
 		_ = []byte(x)
