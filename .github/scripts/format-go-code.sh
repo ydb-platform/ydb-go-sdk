8c69f929b82fb162f5edeb96c8f2e5da060469de
size12 <astarchik1@yandex.ru>
2024-01-29 19:00:10 +0300

    fix :(

--- .github/scripts/format-go-code.sh
+++ .github/scripts/format-go-code.sh
@@ -11,7 +11,7 @@ if [[ "$FILEPATH" == *"tests/slo/"* ]]
 then
   gci write --skip-generated -s standard -s default -s "prefix(slo)" "$FILEPATH"
 elif [ "$FILEPATH" == *"examples/"* ]; then
-  gci write --skip-generated -s standard -s default -s "examples" "$FILEPATH"
+  gci write --skip-generated -s standard -s default -s "prefix(examples)" "$FILEPATH"
 else
   gci write --skip-generated -s standard -s default -s "prefix(github.com/ydb-platform/ydb-go-sdk/v3)" "$FILEPATH"
 fi
