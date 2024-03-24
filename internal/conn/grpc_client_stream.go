c28ddc7f91570360990ee6d19d4073d25283749e
Timofey Koolin <rekby@yandex-team.com>
2024-03-24 14:12:36 +0300

    simplify lastUsage code

--- internal/conn/grpc_client_stream.go
+++ internal/conn/grpc_client_stream.go
@@ -30,7 +30,7 @@ func (s *grpcClientStream) CloseSend() (err error) {
 		onDone(err)
 	}()
 
-	defer s.c.lastUsage.Lock()()
+	defer s.c.lastUsage.SharedLock()()
 
 	err = s.ClientStream.CloseSend()
 
@@ -61,7 +61,7 @@ func (s *grpcClientStream) SendMsg(m interface{}) (err error) {
 		onDone(err)
 	}()
 
-	defer s.c.lastUsage.Lock()()
+	defer s.c.lastUsage.SharedLock()()
 
 	err = s.ClientStream.SendMsg(m)
 
@@ -100,7 +100,7 @@ func (s *grpcClientStream) RecvMsg(m interface{}) (err error) {
 		onDone(err)
 	}()
 
-	defer s.c.lastUsage.Lock()()
+	defer s.c.lastUsage.SharedLock()()
 
 	defer func() {
 		if err != nil {
