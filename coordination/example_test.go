9778cb2db509d082a91e062f6a6b689478d70139
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2024-03-25 14:00:44 +0300

    fix linter issues

--- coordination/example_test.go
+++ coordination/example_test.go
@@ -49,6 +49,7 @@ func Test_semaphore(t *testing.T) {
 	db, err := ydb.Open(ctx, "grpc://localhost:2136/local")
 	if err != nil {
 		fmt.Printf("failed to connect: %v", err)
+
 		return
 	}
 	defer db.Close(ctx) // cleanup resources
@@ -63,12 +64,20 @@ func Test_semaphore(t *testing.T) {
 	})
 	if err != nil {
 		fmt.Printf("failed to create node: %v", err)
+
 		return
 	}
-	defer db.Coordination().DropNode(ctx, "/local/test")
+	defer func() {
+		dropNodeErr := db.Coordination().DropNode(ctx, "/local/test")
+		if dropNodeErr != nil {
+			fmt.Printf("failed to drop node: %v\n", dropNodeErr)
+		}
+	}()
+
 	e, c, err := db.Coordination().DescribeNode(ctx, "/local/test")
 	if err != nil {
 		fmt.Printf("failed to describe node: %v\n", err)
+
 		return
 	}
 	fmt.Printf("node description: %+v\nnode config: %+v\n", e, c)
@@ -76,6 +85,7 @@ func Test_semaphore(t *testing.T) {
 	s, err := db.Coordination().OpenSession(ctx, "/local/test")
 	if err != nil {
 		fmt.Printf("failed to open session: %v\n", err)
+
 		return
 	}
 	defer s.Close(ctx)
@@ -84,6 +94,7 @@ func Test_semaphore(t *testing.T) {
 	err = s.CreateSemaphore(ctx, "my-semaphore", 20, options.WithCreateData([]byte{1, 2, 3}))
 	if err != nil {
 		fmt.Printf("failed to create semaphore: %v", err)
+
 		return
 	}
 	fmt.Printf("semaphore my-semaphore created\n")
@@ -91,9 +102,16 @@ func Test_semaphore(t *testing.T) {
 	lease, err := s.AcquireSemaphore(ctx, "my-semaphore", 10)
 	if err != nil {
 		fmt.Printf("failed to acquire semaphore: %v", err)
+
 		return
 	}
-	defer lease.Release()
+	defer func() {
+		releaseErr := lease.Release()
+		if releaseErr != nil {
+			fmt.Printf("failed to release lease: %v", releaseErr)
+		}
+	}()
+
 	fmt.Printf("session 1 acquired semaphore 10\n")
 
 	s.Reconnect()
@@ -107,6 +125,7 @@ func Test_semaphore(t *testing.T) {
 	)
 	if err != nil {
 		fmt.Printf("failed to describe semaphore: %v", err)
+
 		return
 	}
 	fmt.Printf("session 1 described semaphore %v\n", desc)
@@ -114,6 +133,7 @@ func Test_semaphore(t *testing.T) {
 	err = lease.Release()
 	if err != nil {
 		fmt.Printf("failed to release semaphore: %v", err)
+
 		return
 	}
 	fmt.Printf("session 1 released semaphore my-semaphore\n")
@@ -121,6 +141,7 @@ func Test_semaphore(t *testing.T) {
 	err = s.DeleteSemaphore(ctx, "my-semaphore", options.WithForceDelete(true))
 	if err != nil {
 		fmt.Printf("failed to delete semaphore: %v", err)
+
 		return
 	}
 	fmt.Printf("deleted semaphore my-semaphore\n")
