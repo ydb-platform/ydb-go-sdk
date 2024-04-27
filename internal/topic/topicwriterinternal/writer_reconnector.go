906084eaed3f8307c4a5fa1df604bd1d932ba5d1
Evgenii Shishkov <e.m.shishkov@yandex.ru>
2024-04-27 23:34:41 +0400

    fixing WriterReconnector Close method

--- internal/topic/topicwriterinternal/writer_reconnector.go
+++ internal/topic/topicwriterinternal/writer_reconnector.go
@@ -329,13 +329,17 @@ func (w *WriterReconnector) Flush(ctx context.Context) error {
 	return w.queue.WaitLastWritten(ctx)
 }
 
+//nolint:ifshort //putting flushErr into if scope may cause random fails during closing topicwriter with flush
 func (w *WriterReconnector) Close(ctx context.Context) error {
 	reason := xerrors.WithStackTrace(errStopWriterReconnector)
 	w.queue.StopAddNewMessages(reason)
-	if flushErr := w.Flush(ctx); flushErr != nil {
+
+	flushErr := w.Flush(ctx)
+	closeErr := w.close(ctx, reason)
+
+	if flushErr != nil {
 		return flushErr
 	}
-	closeErr := w.close(ctx, reason)
 
 	return closeErr
 }
