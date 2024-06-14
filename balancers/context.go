453b8a6cfe15b92fa3c1e0cabcb2f780f8410582
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2024-06-14 18:05:55 +0300

    fix linter issue

--- balancers/context.go
+++ balancers/context.go
@@ -6,7 +6,8 @@ import (
 	"github.com/ydb-platform/ydb-go-sdk/v3/internal/endpoint"
 )
 
-// WithNodeID returns the copy of context with NodeID which the client balancer will prefer on step of choose YDB endpoint step
+// WithNodeID returns the copy of context with NodeID which the client balancer will
+// prefer on step of choose YDB endpoint step
 //
 // Experimental: https://github.com/ydb-platform/ydb-go-sdk/blob/master/VERSIONING.md#experimental
 func WithNodeID(ctx context.Context, nodeID uint32) context.Context {
