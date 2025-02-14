715fd8a151dea0550375b593804e0cd03c222180
Semyon Danilov <samvimes@yandex.ru>
2025-02-14 17:21:07 +0400

    Fix again

--- tests/integration/topic_client_test.go
+++ tests/integration/topic_client_test.go
@@ -253,7 +253,6 @@ func TestDescribeTopicConsumer(t *testing.T) {
 		cmpopts.IgnoreFields(topictypes.PartitionConsumerStats{}, "PartitionReadSessionCreateTime", "LastReadTime", "MaxReadTimeLag", "MaxWriteTimeLag"),
 	}
 	for _, p := range consumer.Partitions {
-		require.NotNil(t, p.PartitionConsumerStats.PartitionReadSessionCreateTime)
 		require.NotNil(t, p.PartitionConsumerStats.LastReadTime)
 		require.NotNil(t, p.PartitionConsumerStats.MaxReadTimeLag)
 		require.NotNil(t, p.PartitionConsumerStats.MaxWriteTimeLag)
