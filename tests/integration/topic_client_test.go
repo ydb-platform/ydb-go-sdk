fe968d5112b3ee8d259a9baec14a32a17eeaf4d2
Semyon Danilov <samvimes@yandex.ru>
2025-02-14 18:25:16 +0400

    Fix tests again

--- tests/integration/topic_client_test.go
+++ tests/integration/topic_client_test.go
@@ -256,7 +256,6 @@ func TestDescribeTopicConsumer(t *testing.T) {
 	for _, p := range consumer.Partitions {
 		require.NotNil(t, p.PartitionStats.LastWriteTime)
 
-		require.NotNil(t, p.PartitionConsumerStats.PartitionReadSessionCreateTime)
 		require.NotNil(t, p.PartitionConsumerStats.LastReadTime)
 		require.NotNil(t, p.PartitionConsumerStats.MaxReadTimeLag)
 		require.NotNil(t, p.PartitionConsumerStats.MaxWriteTimeLag)
