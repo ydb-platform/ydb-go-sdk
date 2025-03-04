5911b5097d5f17f56d1797657d544ec1ba45ec1d
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-03-04 12:47:13 +0300

    added test and fixes

--- internal/xruntime/cleanup.go
+++ internal/xruntime/cleanup.go
@@ -5,7 +5,7 @@ package xruntime
 import "runtime"
 
 func AddCleanup[T, S any](ptr *T, cleanup func(S), arg S) {
-	runtime.SetFinalizer(ptr, func() {
+	runtime.SetFinalizer(ptr, func(ptr *T) {
 		cleanup(arg)
 	})
 }
