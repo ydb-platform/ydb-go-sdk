afc803b9affbe1121585f76d6d98095fa27f9fe9
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-03-04 19:51:15 +0300

    fixed TestDatabaseSqlMetrics and helpers SQLDriver

--- tests/integration/helpers_test.go
+++ tests/integration/helpers_test.go
@@ -145,7 +145,7 @@ func (scope *scopeT) driverNamed(name string, opts ...ydb.Option) *ydb.Driver {
 func (scope *scopeT) SQLDriver(opts ...ydb.ConnectorOption) *sql.DB {
 	f := func() (*fixenv.GenericResult[*sql.DB], error) {
 		driver := scope.Driver()
-		scope.Logf("Create sql db connector")
+		scope.Logf("Create database/sql connector for YDB")
 		connector, err := ydb.Connector(driver, opts...)
 		if err != nil {
 			return nil, err
@@ -153,11 +153,9 @@ func (scope *scopeT) SQLDriver(opts ...ydb.ConnectorOption) *sql.DB {
 
 		db := sql.OpenDB(connector)
 
-		clean := func() {
+		return fixenv.NewGenericResultWithCleanup(db, func() {
 			scope.Require.NoError(db.Close())
-		}
-
-		return fixenv.NewGenericResultWithCleanup(db, clean), nil
+		}), nil
 	}
 	return fixenv.CacheResult(scope.Env, f)
 }
