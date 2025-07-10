60b178fa9004b6fcc2f445e04bae57ffa3c857e4
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-07-10 21:04:17 +0300

    Update config/config.go

--- config/config.go
+++ config/config.go
@@ -311,7 +311,8 @@ func ExcludeGRPCCodesForPessimization(codes ...grpcCodes.Code) Option {
 	}
 }
 
-// WithDisableOptimisticUnban disables optimistic unban of nodes after a successful call immediately following pessimization.
+// WithDisableOptimisticUnban disables optimistic unban of nodes after a successful call 
+// immediately following pessimization.
 //
 // Experimental: https://github.com/ydb-platform/ydb-go-sdk/blob/master/VERSIONING.md#experimental
 func WithDisableOptimisticUnban() Option {
