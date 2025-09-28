ba7a2261b985dcdcab54fa9936a2d8b1fe5a9122
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-09-28 12:21:50 +0300

    Fix linter issue

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -8,10 +8,11 @@ import (
 
 var (
 	// DefaultKeepaliveInterval contains default duration between grpc keepalive
-	DefaultKeepaliveInterval    = 10 * time.Second
-	MinKeepaliveInterval        = 10 * time.Second
-	DefaultDialTimeout          = 5 * time.Second
-	DefaultGRPCMsgSize          = 64000000 // 64 million bytes, same as server default: https://github.com/ydb-platform/ydb/blob/19a39b96b1a403d2ab230d1f4e1c9324cb71e047/ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/grpc_common/constants.h#L7 //nolint:lll
+	DefaultKeepaliveInterval = 10 * time.Second
+	MinKeepaliveInterval     = 10 * time.Second
+	DefaultDialTimeout       = 5 * time.Second
+	//nolint:lll
+	DefaultGRPCMsgSize = 64000000 // 64 million bytes, same as server default: https://github.com/ydb-platform/ydb/blob/19a39b96b1a403d2ab230d1f4e1c9324cb71e047/ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/grpc_common/constants.h#L7
 	DefaultGrpcConnectionPolicy = keepalive.ClientParameters{
 		Time:                DefaultKeepaliveInterval,
 		Timeout:             MinKeepaliveInterval,
