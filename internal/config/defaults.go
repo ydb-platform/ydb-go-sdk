a12737f9a28036551454248e051e085cfb1a424b
Konstantin Prokopenko <kprokopenko@users.noreply.github.com>
2025-09-30 14:52:36 +0300

    fix on fix lint

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -12,7 +12,7 @@ var (
 	MinKeepaliveInterval     = 10 * time.Second
 	DefaultDialTimeout       = 5 * time.Second
 	//nolint:lll
-	DefaultGRPCMsgSize = 64000000 // 64 million bytes, same as server default: https://github.com/ydb-platform/ydb/blob/19a39b96b1a403d2ab230d1f4e1c9324cb71e047/ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/grpc_common/constants.h#L7
+	DefaultGRPCMsgSize          = 64000000 // 64 million bytes, same as server default: https://github.com/ydb-platform/ydb/blob/19a39b96b1a403d2ab230d1f4e1c9324cb71e047/ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/grpc_common/constants.h#L7
 	DefaultGrpcConnectionPolicy = keepalive.ClientParameters{
 		Time:                DefaultKeepaliveInterval,
 		Timeout:             MinKeepaliveInterval,
