d2f30c7e59ae07b62cbbe7c2783b3f699bbca31e
Konstantin Prokopenko <kprokopenko@users.noreply.github.com>
2025-10-22 21:07:26 +0300

    add changelog and tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
+* Added support for `Result.RowsAffected()` for `database/sql`
+
 ## v3.117.1
 * Fixed scan a column of type `Decimal(precision,scale)` into a struct field of type `types.Decimal{}` using `ScanStruct()`
 * Fixed race in integration test `TestTopicWriterLogMessagesWithoutData`
--- tests/integration/database_sql_rows_affected_test.go
+++ tests/integration/database_sql_rows_affected_test.go
@@ -9,10 +9,37 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/ydb-platform/ydb-go-sdk/v3"
 )
 
 func TestDatabaseSQLRowsAffected(t *testing.T) {
+	tests := []struct {
+		sql  string
+		rows int64
+	}{
+		{
+			sql:  "INSERT INTO %s (id) values (1),(2),(3)",
+			rows: 3,
+		},
+		{
+			sql:  "UPDATE %s SET val = 'test' where id > 1",
+			rows: 2,
+		},
+		{
+			sql:  "DELETE FROM %s",
+			rows: 3,
+		},
+		{
+			sql:  "INSERT INTO %s (id) values (1),(2),(3); INSERT INTO %[1]s (id) values (4),(5); ",
+			rows: 5,
+		},
+		{
+			sql:  "UPDATE %s SET val = 'test' where id > 1; DELETE FROM %[1]s WHERE id < 4", // 4+3
+			rows: 7,
+		},
+	}
+
 	var (
 		scope = newScope(t)
 		db    = scope.SQLDriverWithFolder(ydb.WithQueryService(true))
@@ -22,11 +49,15 @@ func TestDatabaseSQLRowsAffected(t *testing.T) {
 		_ = db.Close()
 	}()
 
-	result, err := db.Exec(fmt.Sprintf("INSERT INTO %s (id) values (1),(2),(3) ", scope.TableName()))
-	require.NoError(t, err)
+	for _, test := range tests {
+		t.Run(test.sql, func(t *testing.T) {
+			result, err := db.Exec(fmt.Sprintf(test.sql, scope.TableName()))
+			require.NoError(t, err)
 
-	got, err := result.RowsAffected()
-	require.NoError(t, err)
+			got, err := result.RowsAffected()
+			require.NoError(t, err)
 
-	assert.Equal(t, int64(3), got)
+			assert.Equal(t, test.rows, got)
+		})
+	}
 }
