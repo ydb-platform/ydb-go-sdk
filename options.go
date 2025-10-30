ef3166772120992aa2c3b8fa988f2a1c00446957
Konstantin Prokopenko <kprokopenko@users.noreply.github.com>
2025-10-30 16:06:56 +0300

    revert broken changes

--- options.go
+++ options.go
@@ -2,6 +2,7 @@ package ydb
 
 import (
 	"context"
+	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"os"
@@ -482,6 +483,18 @@ func WithCertificatesFromFile(caFile string, opts ...certificates.FromFileOption
 				return xerrors.WithStackTrace(err)
 			}
 		}
+
+		return nil
+	}
+}
+
+// WithTLSConfig replaces older TLS config
+//
+// Warning: all early TLS config changes (such as WithCertificate, WithCertificatesFromFile, WithCertificatesFromPem,
+// WithMinTLSVersion, WithTLSSInsecureSkipVerify) will be lost
+func WithTLSConfig(tlsConfig *tls.Config) Option {
+	return func(ctx context.Context, d *Driver) error {
+		d.options = append(d.options, config.WithTLSConfig(tlsConfig))
 
 		return nil
 	}
--- query/apache_arrow.go
+++ query/apache_arrow.go
@@ -1,11 +1,20 @@
 package query
 
 import (
+	"context"
+
 	"github.com/ydb-platform/ydb-go-sdk/v3/internal/query/arrow"
 )
 
 // ArrowExecutor is an interface for execute queries with results in `Apache Arrow` format.
 type ArrowExecutor interface {
+	// QueryArrow like [Executor.Query] but returns results in [Apache Arrow] format.
+	// Each part of the result implements io.Reader and contains the data in Arrow IPC format.
+	//
+	// Experimental: https://github.com/ydb-platform/ydb-go-sdk/blob/master/VERSIONING.md#experimental
+	//
+	// [Apache Arrow]: https://arrow.apache.org/
+	QueryArrow(ctx context.Context, sql string, opts ...ExecuteOption) (ArrowResult, error)
 }
 
 type ArrowResult = arrow.Result
