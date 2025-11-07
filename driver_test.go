7663332fd07824e4384515399f757ceb71e024bc
Konstantin Prokopenko <kprokopenko@users.noreply.github.com>
2025-11-07 19:33:11 +0300

    fix lint

--- driver_test.go
+++ driver_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
 	"github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/trace"
 )
