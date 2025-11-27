93691acc52865a2d8b2cbde78cd96ef117296915
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-11-27 11:26:03 +0300

    Apply suggestions from code review

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -876,13 +876,13 @@ func (p *Pool[PT, T]) getItem(ctx context.Context) (item PT, finalErr error) { /
 	p.mu.RLock()
 	defer p.mu.RUnlock()
 
+	msg := fmt.Sprintf(
+		"failed to get item from pool after %d attempts and %v, pool has %d items (%d busy, %d idle, %d create_in_progress)", //nolint:lll
+		attempt, p.config.clock.Since(start), len(p.index),
+		len(p.index)-p.idle.Len(), p.idle.Len(), p.createInProgress,
+	)
 	if lastErr != nil {
-		return nil, xerrors.WithStackTrace(
-			fmt.Errorf("failed to get item from pool after %d attempts and %v, pool has %d items (%d busy, "+
-				"%d idle, %d create_in_progress): %w", attempt, p.config.clock.Since(start), len(p.index),
-				len(p.index)-p.idle.Len(), p.idle.Len(), p.createInProgress, lastErr,
-			),
-		)
+		return nil, xerrors.WithStackTrace(fmt.Errorf(msg + ": %w", lastErr))
 	}
 
 	return nil, xerrors.WithStackTrace(
