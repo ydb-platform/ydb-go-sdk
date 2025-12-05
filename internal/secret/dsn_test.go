c1128f56a682e72c8b335bf8caea7613d5a22ad5
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2025-12-05 19:25:55 +0300

    fixes

--- internal/secret/dsn_test.go
+++ internal/secret/dsn_test.go
@@ -8,36 +8,36 @@ import (
 
 func TestDSN(t *testing.T) {
 	for _, tt := range []struct {
-		src string
-		res string
+		dsn string
+		exp string
 	}{
 		{
-			src: "not-a-valid-url",
-			res: "<invalid DSN>",
+			dsn: "grpc://192.168.0.%31:2136/",
+			exp: "<invalid DSN>",
 		},
 		{
-			src: "grpc://debuguser:debugpassword@localhost:2136/local1",
-			res: "grpc://localhost:2136/local1",
+			dsn: "grpc://debuguser:debugpassword@localhost:2136/local1",
+			exp: "grpc://localhost:2136/local1",
 		},
 		{
-			src: "grpc://localhost:2136/local1?user=debuguser&password=debugpassword",
-			res: "grpc://localhost:2136/local1",
+			dsn: "grpc://localhost:2136/local1?user=debuguser&password=debugpassword",
+			exp: "grpc://localhost:2136/local1",
 		},
 		{
-			src: "grpc://localhost:2136/local1?login=debuguser&password=debugpassword",
-			res: "grpc://localhost:2136/local1",
+			dsn: "grpc://localhost:2136/local1?login=debuguser&password=debugpassword",
+			exp: "grpc://localhost:2136/local1",
 		},
 		{
-			src: "grpc://localhost:2136/local1?param1=value1&login=debuguser&param2=value2&password=debugpassword&param2=value3",
-			res: "grpc://localhost:2136/local1?param1=value1&param2=value2&param2=value3",
+			dsn: "grpc://localhost:2136/local1?param1=value1&login=debuguser&param2=value2&password=debugpassword&param2=value3",
+			exp: "grpc://localhost:2136/local1?param1=value1&param2=value2&param2=value3",
 		},
 		{
-			src: "grpc://localhost:2136/local1?token=secrettoken123",
-			res: "grpc://localhost:2136/local1",
+			dsn: "grpc://localhost:2136/local1?token=secrettoken123",
+			exp: "grpc://localhost:2136/local1",
 		},
 	} {
-		t.Run(tt.src, func(t *testing.T) {
-			require.Equal(t, tt.res, DSN(tt.src))
+		t.Run(tt.dsn, func(t *testing.T) {
+			require.Equal(t, tt.exp, DSN(tt.dsn))
 		})
 	}
 }
