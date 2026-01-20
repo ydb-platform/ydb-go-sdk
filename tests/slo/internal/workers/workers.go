739ffb3180df26b36c8b411bc7004628e0c8e884
Aleksei Pleshakov <qrort@ydb.tech>
2026-01-20 19:27:30 +0100

    session pool node hints SLO

--- tests/slo/internal/workers/workers.go
+++ tests/slo/internal/workers/workers.go
@@ -15,20 +15,57 @@ type ReadWriter interface {
 	Write(ctx context.Context, row generator.Row) (attempts int, err error)
 }
 
+type ReadWriterNodeHints interface {
+	Read(ctx context.Context, rowID generator.RowID) (_ generator.Row, attempts int, missed bool, err error)
+	Write(ctx context.Context, row generator.Row) (attempts int, missed bool, err error)
+}
+
 type BatchReadWriter interface {
 	ReadBatch(ctx context.Context, rowIDs []generator.RowID) (_ []generator.Row, attempts int, err error)
 	WriteBatch(ctx context.Context, rows []generator.Row) (attempts int, err error)
 }
 
+type MissesWrapper struct {
+	readWriter ReadWriter
+}
+
+func (w *MissesWrapper) Read(ctx context.Context, rowID generator.RowID) (generator.Row, int, bool, error) {
+	row, attempts, err := w.readWriter.Read(ctx, rowID)
+	return row, attempts, false, err
+}
+func (w *MissesWrapper) Write(ctx context.Context, row generator.Row) (int, bool, error) {
+	attempts, err := w.readWriter.Write(ctx, row)
+	return attempts, false, err
+}
+
+func wrapWithMisses(s ReadWriter) ReadWriterNodeHints {
+	return &MissesWrapper{readWriter: s}
+}
+
 type Workers struct {
 	cfg *config.Config
-	s   ReadWriter
+	s   ReadWriterNodeHints
 	sb  BatchReadWriter
 	m   *metrics.Metrics
 	Gen generator.Generator
 }
 
 func New(cfg *config.Config, s ReadWriter, ref, label, jobName string) (*Workers, error) {
+	m, err := metrics.New(cfg.OTLPEndpoint, ref, label, jobName, cfg.ReportPeriod)
+	if err != nil {
+		log.Printf("create metrics failed: %v", err)
+
+		return nil, err
+	}
+
+	return &Workers{
+		cfg: cfg,
+		s:   wrapWithMisses(s),
+		m:   m,
+	}, nil
+}
+
+func NewWithNodeHints(cfg *config.Config, s ReadWriterNodeHints, ref, label, jobName string) (*Workers, error) {
 	m, err := metrics.New(cfg.OTLPEndpoint, ref, label, jobName, cfg.ReportPeriod)
 	if err != nil {
 		log.Printf("create metrics failed: %v", err)
---
+++ tests/slo/native/session_node_hints/main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/time/rate"
+
+	"slo/internal/config"
+	"slo/internal/generator"
+	"slo/internal/log"
+	"slo/internal/workers"
+)
+
+var (
+	ref     string
+	label   string
+	jobName string
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer cancel()
+
+	cfg, err := config.New()
+	if err != nil {
+		panic(fmt.Errorf("create config failed: %w", err))
+	}
+
+	log.Println("program started")
+	defer log.Println("program finished")
+
+	ctx, cancel = context.WithTimeout(ctx, time.Duration(cfg.Time)*time.Second)
+	defer cancel()
+
+	go func() {
+		<-ctx.Done()
+		log.Println("exiting...")
+	}()
+
+	// pool size similar to query variant
+	s, err := NewStorage(ctx, cfg, cfg.ReadRPS+cfg.WriteRPS, label)
+	if err != nil {
+		panic(fmt.Errorf("create storage failed: %w", err))
+	}
+	defer func() {
+		var (
+			shutdownCtx    context.Context
+			shutdownCancel context.CancelFunc
+		)
+		if cfg.ShutdownTime > 0 {
+			shutdownCtx, shutdownCancel = context.WithTimeout(context.Background(),
+				time.Duration(cfg.ShutdownTime)*time.Second)
+		} else {
+			shutdownCtx, shutdownCancel = context.WithCancel(context.Background())
+		}
+		defer shutdownCancel()
+
+		_ = s.Close(shutdownCtx)
+	}()
+
+	log.Println("db init ok")
+	gen := generator.NewSeeded(120394832798)
+<<<<<<< Updated upstream
+	//gen := generator.New(0)
+=======
+>>>>>>> Stashed changes
+	switch cfg.Mode {
+	case config.CreateMode:
+		err = s.CreateTable(ctx)
+		if err != nil {
+			panic(fmt.Errorf("create table failed: %w", err))
+		}
+		log.Println("create table ok")
+
+		g := errgroup.Group{}
+
+		for i := uint64(0); i < cfg.InitialDataCount; i++ {
+			g.Go(func() (err error) {
+				e, err := gen.Generate()
+				if err != nil {
+					return err
+				}
+
+				_, _, err = s.Write(ctx, e)
+				if err != nil {
+					return err
+				}
+
+				return nil
+			})
+		}
+
+		err = g.Wait()
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("entries write ok")
+	case config.CleanupMode:
+		err = s.DropTable(ctx)
+		if err != nil {
+			panic(fmt.Errorf("create table failed: %w", err))
+		}
+
+		log.Println("cleanup table ok")
+	case config.RunMode:
+		// to wait for correct partitions boundaries
+		time.Sleep(10 * time.Second)
+		w, err := workers.NewWithNodeHints(cfg, s, ref, label, jobName)
+		if err != nil {
+			panic(fmt.Errorf("create workers failed: %w", err))
+		}
+<<<<<<< Updated upstream
+		//ns := s.nodeSelector.Load()
+		//idx, nodeID := ns.GetRandomNodeID(gen)
+		//log.Println("all requests to node id: ", nodeID)
+		//gen.SetRange(ns.LowerBounds[idx], ns.UpperBounds[idx])
+=======
+>>>>>>> Stashed changes
+		w.Gen = gen
+		defer func() {
+			err := w.Close()
+			if err != nil {
+				panic(fmt.Errorf("workers close failed: %w", err))
+			}
+			log.Println("workers close ok")
+		}()
+
+		// run workers
+		wg := sync.WaitGroup{}
+		readRL := rate.NewLimiter(rate.Limit(cfg.ReadRPS), 1)
+		wg.Add(cfg.ReadRPS)
+		for i := 0; i < cfg.ReadRPS; i++ {
+			go w.Read(ctx, &wg, readRL)
+		}
+		log.Println("started " + strconv.Itoa(cfg.ReadRPS) + " read workers")
+
+		writeRL := rate.NewLimiter(rate.Limit(cfg.WriteRPS), 1)
+		wg.Add(cfg.WriteRPS)
+		for i := 0; i < cfg.WriteRPS; i++ {
+			go w.Write(ctx, &wg, writeRL, gen)
+		}
+		log.Println("started " + strconv.Itoa(cfg.WriteRPS) + " write workers")
+		wg.Wait()
+	default:
+		panic(fmt.Errorf("unknown mode: %v", cfg.Mode))
+	}
+}
