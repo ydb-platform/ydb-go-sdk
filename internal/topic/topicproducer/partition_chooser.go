ed85c2e19dbc8c94df9da442cdf9a5ce9ec49f66
Roman Kuzin <kuzin57@yandex-team.ru>
2026-02-22 00:00:33 +0300

    LOGBROKER-10206 Better

--- internal/topic/topicproducer/partition_chooser.go
+++ internal/topic/topicproducer/partition_chooser.go
@@ -2,25 +2,34 @@ package topicproducer
 
 import "github.com/spaolacci/murmur3"
 
+var (
+	_ PartitionChooser = (*boundPartitionChooser)(nil)
+	_ PartitionChooser = (*hashPartitionChooser)(nil)
+)
+
 type PartitionChooser interface {
-	ChoosePartition(key string) int64
+	ChoosePartition(key string) (int64, error)
 }
 
 type boundPartitionChooser struct {
 	cfg *ProducerConfig
 }
 
-func (c *boundPartitionChooser) ChoosePartition(key string) int64 {
-	return 0
+func (c *boundPartitionChooser) ChoosePartition(key string) (int64, error) {
+	return 0, nil
 }
 
 type hashPartitionChooser struct {
 	cfg        *ProducerConfig
 	hasher     murmur3.Hash128
-	partitions int64
+	partitions uint64
 }
 
-func (c *hashPartitionChooser) ChoosePartition(key string) int64 {
-	low, high := c.hasher.Sum128([]byte(key))
-	return int64((low + high) % c.partitions)
+func (c *hashPartitionChooser) ChoosePartition(key string) (int64, error) {
+	const seed = 111111
+
+	hasher := murmur3.New128()
+	hasher.Write([]byte(key))
+	low, high := c.hasher.Sum128()
+	return int64((low + high*seed) % c.partitions), nil
 }
