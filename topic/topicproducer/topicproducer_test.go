ad4720e6a8f0447bf16138213cae594264e56a4f
Roman Kuzin <kuzin57@yandex-team.ru>
2026-03-02 15:36:27 +0000

    LOGBROKER-10206 Better

---
+++ topic/topicproducer/topicproducer_test.go
@@ -0,0 +1,40 @@
+package topicproducer_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/ydb-platform/ydb-go-sdk/v3"
+	"github.com/ydb-platform/ydb-go-sdk/v3/topic/topicoptions"
+	"github.com/ydb-platform/ydb-go-sdk/v3/topic/topicproducer"
+)
+
+func ExampleProducer_Write() {
+	ctx := context.Background()
+	db, err := ydb.Open(ctx, os.Getenv("YDB_CONNECTION_STRING"))
+	if err != nil {
+		log.Fatalf("failed ydb connection: %v", err)
+	}
+
+	producer, err := db.Topic().CreateProducer("topicName", topicoptions.WithProducerIDPrefix("test-producer"))
+	if err != nil {
+		log.Fatalf("failed to create topic producer: %v", err)
+	}
+
+	msg1 := topicproducer.Message{Key: "key-1"}
+	msg1.Data = strings.NewReader("1")
+	msg2 := topicproducer.Message{Key: "key-2"}
+	msg2.Data = strings.NewReader("2")
+	msg3 := topicproducer.Message{Key: "key-3"}
+	msg3.Data = strings.NewReader("3")
+
+	err = producer.Write(ctx, msg1, msg2, msg3)
+	if err != nil {
+		fmt.Println("OK")
+	} else {
+		log.Fatalf("failed write to topic producer")
+	}
+}
