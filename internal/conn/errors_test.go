6bab148ee41f2696a6340457799e27a90035c03c
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-03-17 11:50:00 +0300

    fix

--- internal/conn/errors_test.go
+++ internal/conn/errors_test.go
@@ -17,125 +17,124 @@ import (
 func TestIsBadConn(t *testing.T) {
 	ctx := xtest.Context(t)
 
-	t.Run("ExcludeCodeFromBadCodes", func(t *testing.T) {
-		for i, tt := range []struct {
-			err           error
-			goodConnCodes []grpcCodes.Code
-			badConn       bool
-		}{
-			{
-				err:     fmt.Errorf("test"),
-				badConn: false,
+	for i, tt := range []struct {
+		err           error
+		goodConnCodes []grpcCodes.Code
+		badConn       bool
+	}{
+		{
+			err:     fmt.Errorf("test"),
+			badConn: false,
+		},
+		{
+			err:     xerrors.Operation(),
+			badConn: false,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.ResourceExhausted, "")),
+			badConn: false,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unavailable, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.OK, "")),
+			badConn: false,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Canceled, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unknown, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.InvalidArgument, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.DeadlineExceeded, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.NotFound, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.AlreadyExists, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.PermissionDenied, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.FailedPrecondition, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Aborted, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.OutOfRange, "")),
+			badConn: false,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unimplemented, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Internal, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.DataLoss, "")),
+			badConn: true,
+		},
+		{
+			err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unauthenticated, "")),
+			badConn: true,
+		},
+		{
+			err: xerrors.Transport(grpcStatus.Error(grpcCodes.Unauthenticated, "")),
+			goodConnCodes: []grpcCodes.Code{
+				grpcCodes.Unauthenticated,
 			},
-			{
-				err:     xerrors.Operation(),
-				badConn: false,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.ResourceExhausted, "")),
-				badConn: false,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unavailable, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.OK, "")),
-				badConn: false,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Canceled, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unknown, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.InvalidArgument, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.DeadlineExceeded, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.NotFound, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.AlreadyExists, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.PermissionDenied, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.FailedPrecondition, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Aborted, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.OutOfRange, "")),
-				badConn: false,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unimplemented, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Internal, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.DataLoss, "")),
-				badConn: true,
-			},
-			{
-				err:     xerrors.Transport(grpcStatus.Error(grpcCodes.Unauthenticated, "")),
-				badConn: true,
-			},
-			{
-				err: xerrors.Transport(grpcStatus.Error(grpcCodes.Unauthenticated, "")),
-				goodConnCodes: []grpcCodes.Code{
-					grpcCodes.Unauthenticated,
-				},
-				badConn: false,
-			},
-		} {
-			t.Run(fmt.Sprintf("%d. %v", i, tt.err), func(t *testing.T) {
-				require.Equal(t, tt.badConn, IsBadConn(ctx, tt.err, tt.goodConnCodes...))
-				require.Equal(t, tt.badConn, IsBadConn(ctx, xerrors.WithStackTrace(tt.err), tt.goodConnCodes...))
-				require.Equal(t, tt.badConn, IsBadConn(ctx, xerrors.Retryable(tt.err), tt.goodConnCodes...))
-			})
-		}
-	})
+			badConn: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d. %v", i, tt.err), func(t *testing.T) {
+			require.Equal(t, tt.badConn, IsBadConn(ctx, tt.err, tt.goodConnCodes...))
+			require.Equal(t, tt.badConn, IsBadConn(ctx, xerrors.WithStackTrace(tt.err), tt.goodConnCodes...))
+			require.Equal(t, tt.badConn, IsBadConn(ctx, xerrors.Retryable(tt.err), tt.goodConnCodes...))
+		})
+	}
+}
 
-	t.Run("BanOnOperationError", func(t *testing.T) {
-		// Ban on operation error ABORTED only
-		ctx = BanOnOperationError(ctx, Ydb.StatusIds_ABORTED)
-		require.False(t, IsBadConn(ctx, nil))
-		require.False(t, IsBadConn(ctx, errors.New("test")))
-		require.False(t, IsBadConn(ctx, xerrors.WithStackTrace(
-			xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_OVERLOADED))),
-		))
-		require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
-			xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_ABORTED))),
-		))
+func TestBanOnOperationError(t *testing.T) {
+	ctx := xtest.Context(t)
+	// Ban on operation error ABORTED only
+	ctx = BanOnOperationError(ctx, Ydb.StatusIds_ABORTED)
+	require.False(t, IsBadConn(ctx, nil))
+	require.False(t, IsBadConn(ctx, errors.New("test")))
+	require.False(t, IsBadConn(ctx, xerrors.WithStackTrace(
+		xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_OVERLOADED))),
+	))
+	require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
+		xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_ABORTED))),
+	))
 
-		// Add OVERLOADED to operation error codes
-		ctx = BanOnOperationError(ctx, Ydb.StatusIds_OVERLOADED)
-		require.False(t, IsBadConn(ctx, nil))
-		require.False(t, IsBadConn(ctx, errors.New("test")))
-		require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
-			xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_OVERLOADED))),
-		))
-		require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
-			xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_ABORTED))),
-		))
-	})
+	// Add OVERLOADED to operation error codes
+	ctx = BanOnOperationError(ctx, Ydb.StatusIds_OVERLOADED)
+	require.False(t, IsBadConn(ctx, nil))
+	require.False(t, IsBadConn(ctx, errors.New("test")))
+	require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
+		xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_OVERLOADED))),
+	))
+	require.True(t, IsBadConn(ctx, xerrors.WithStackTrace(
+		xerrors.Operation(xerrors.WithStatusCode(Ydb.StatusIds_ABORTED))),
+	))
 }
