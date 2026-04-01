edfb1a58b9388ee6f78abefc00648794bcfa4fd3
Roman Kuzin <kuzin57@yandex-team.ru>
2026-04-01 09:24:25 +0000

    LOGBROKER-10309 Fix

--- internal/topic/topicmultiwriter/orchestrator.go
+++ internal/topic/topicmultiwriter/orchestrator.go
@@ -189,6 +189,7 @@ func (o *orchestrator) choosePartition(msg message) (partitionID int64, err erro
 	return partitionID, nil
 }
 
+//nolint:funlen
 func (o *orchestrator) pushMessage(ctx context.Context, msg message) (err error) {
 	acquired := false
 	defer func() {
