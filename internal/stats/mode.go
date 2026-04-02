a17cf44e4f02c36df0acededf0219f790e52f5b1
Konstantin Prokopenko <kprokopenko@users.noreply.github.com>
2026-04-02 19:43:46 +0300

    fix

--- internal/stats/mode.go
+++ internal/stats/mode.go
@@ -6,9 +6,7 @@ package stats
 type Mode int32
 
 const (
-	ModeUnspecified Mode = 0
-	ModeNode        Mode = 10 // Stats collection is disabled
-	ModeBasic       Mode = 20 // Aggregated stats of reads, updates and deletes per table
-	ModeFull        Mode = 30 // Add execution stats and plan on top of [ModeBasic]
-	ModeProfile     Mode = 40 // Detailed execution stats including stats for individual tasks and channels
+	ModeBasic   Mode = 20 // Aggregated stats of reads, updates and deletes per table
+	ModeFull    Mode = 30 // Add execution stats and plan on top of [ModeBasic]
+	ModeProfile Mode = 40 // Detailed execution stats including stats for individual tasks and channels
 )
