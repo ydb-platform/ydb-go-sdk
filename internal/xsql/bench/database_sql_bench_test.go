40155842d631eb9f1cb4021bbb495af3cdfa579b
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-05-06 15:27:24 +0300

    bench results

--- internal/xsql/bench/database_sql_bench_test.go
+++ internal/xsql/bench/database_sql_bench_test.go
@@ -33,12 +33,12 @@ import (
 // Before PR
 // BenchmarkDatabaseSQLMock/QueryService-12         	   82140	     12720 ns/op	   27862 B/op	     471 allocs/op
 // BenchmarkDatabaseSQLMock/TableService-12         	  105169	      9726 ns/op	   19381 B/op	     323 allocs/op
-// Diff														28%				30%				43%					45%
+// Diff														-21%				30%				43%					45%
 
 // After PR
-// BenchmarkDatabaseSQLMock/QueryService-12         	   80618	     12783 ns/op	   27482 B/op	     464 allocs/op
-// BenchmarkDatabaseSQLMock/TableService-12         	  130173	      9187 ns/op	   19173 B/op	     320 allocs/op
-// Diff													 61% (+5%)		  39% (+9%)	 	    43% (0%)		 45% (0%)
+// BenchmarkDatabaseSQLMock/QueryService-12    834153     12112 ns/op   26594 B/op   440 allocs/op
+// BenchmarkDatabaseSQLMock/TableService-12   1312279     11599 ns/op   18598 B/op   307 allocs/op
+// Diff (quary/table*100-100)				-36% (-15%)		4% (-26%)	42% (-1%)	 43% (-2%)
 
 // grpcMockYDB is a local in-process gRPC stack (Discovery + Table + Query) with fixed
 // "SELECT 42" style responses. It does not require a real YDB endpoint.
