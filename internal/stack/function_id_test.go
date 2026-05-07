0b4a9194a62c1c2356653ac8a35fac8b95d88ad9
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-05-07 14:55:26 +0300

    fix

--- internal/stack/function_id_test.go
+++ internal/stack/function_id_test.go
@@ -78,8 +78,8 @@ func TestFunctionIDForGenericType(t *testing.T) {
 	})
 }
 
-// BenchmarkFunctionID/stack.FunctionID_literal-12     ~0 ns/op  0 B/op  0 allocs/op
-// BenchmarkFunctionID/stack.RuntimeFunctionID-12  ~11 ns/op 16 B/op  1 alloc/op (non-empty delegates to literal)
+// BenchmarkFunctionID/stack.FunctionID.literal-12  0.2677 ns/op	  0 B/op	0 allocs/op
+// BenchmarkFunctionID/stack.RuntimeFunctionID-12   11.51 ns/op	  16 B/op	1 allocs/op
 func BenchmarkFunctionID(b *testing.B) {
 	b.Run("stack.FunctionID.literal", func(b *testing.B) {
 		b.ReportAllocs()
