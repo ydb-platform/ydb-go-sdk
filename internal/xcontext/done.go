f620149b5ed95ed30dd130168d9f97b7f0fde88b
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-05-08 19:11:31 +0300

    fix

--- internal/xcontext/done.go
+++ internal/xcontext/done.go
@@ -2,7 +2,6 @@ package xcontext
 
 import (
 	"context"
-	"time"
 )
 
 var (
@@ -18,29 +17,21 @@ var (
 // doneAlreadySignaledCtx is used when done is readable at WithDone entry so we
 // avoid context.WithCancel and context.AfterFunc (cheaper fast path).
 type doneAlreadySignaledCtx struct {
-	parent context.Context //nolint:containedctx // thin wrapper delegating Deadline/Value
+	context.Context //nolint:containedctx // thin wrapper delegating Deadline/Value
 }
 
-func (c *doneAlreadySignaledCtx) Deadline() (deadline time.Time, ok bool) {
-	return c.parent.Deadline()
-}
-
-func (c *doneAlreadySignaledCtx) Done() <-chan struct{} {
+func (doneAlreadySignaledCtx) Done() <-chan struct{} {
 	return closedDoneChan
 }
 
-func (c *doneAlreadySignaledCtx) Err() error {
+func (doneAlreadySignaledCtx) Err() error {
 	return context.Canceled
-}
-
-func (c *doneAlreadySignaledCtx) Value(key any) any {
-	return c.parent.Value(key)
 }
 
 func WithDone(parent context.Context, done <-chan struct{}) (context.Context, context.CancelFunc) {
 	select {
 	case <-done:
-		return &doneAlreadySignaledCtx{parent: parent}, noopCancel
+		return doneAlreadySignaledCtx{Context: parent}, noopCancel
 	default:
 		ctx, cancel := context.WithCancel(parent)
 
