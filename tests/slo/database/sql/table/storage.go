62b8a5a89d06f6b7c17cb461e197b9f7a5e63c63
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-06-14 17:34:41 +0300

    Update storage.go

--- tests/slo/database/sql/table/storage.go
+++ tests/slo/database/sql/table/storage.go
@@ -99,8 +99,8 @@ func NewStorage(ctx context.Context, fw *framework.Framework) (framework.Workloa
 	}
 
 	sqlDB := sql.OpenDB(connector)
-	sqlDB.SetMaxOpenConns(params.PoolSize())
-	sqlDB.SetMaxIdleConns(params.PoolSize())
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetMaxIdleConns(100)
 	sqlDB.SetConnMaxIdleTime(time.Second)
 
 	return kv.New(fw, params, &db{
