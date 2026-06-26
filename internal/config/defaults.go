68d214e3f681fd2736ba16ec425863d16a02c216
Aleksey Myasnikov <asmyasnikov@ydb.tech>
2026-06-26 08:43:09 +0300

    decrease default dial timeout from 5 sec to 500 msec

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -10,7 +10,7 @@ var (
 	// DefaultKeepaliveInterval contains default duration between grpc keepalive
 	DefaultKeepaliveInterval = 10 * time.Second
 	MinKeepaliveInterval     = 10 * time.Second
-	DefaultDialTimeout       = 5 * time.Second
+	DefaultDialTimeout       = 500 * time.Millisecond
 	//nolint:lll
 	DefaultGRPCMsgSize          = 64000000 // 64 million bytes, same as server default: https://github.com/ydb-platform/ydb/blob/19a39b96b1a403d2ab230d1f4e1c9324cb71e047/ydb/public/sdk/cpp/include/ydb-cpp-sdk/library/grpc_common/constants.h#L7
 	DefaultGrpcConnectionPolicy = keepalive.ClientParameters{
